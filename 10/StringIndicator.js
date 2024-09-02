8339d867555b2b99c11de53e602332663dd159ed
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-09-02 13:13:33 +0900

    added string control/indicator in 10

---
+++ 10/StringIndicator.js
@@ -0,0 +1,23 @@
+import AssignableString from './AssignableString.js'
+
+export default class extends AssignableString {
+    constructor(channel, webSockets, connectedWebSocket) {
+        super()
+        this._value
+        this._channel = channel
+        this._webSockets
+        webSockets.prependListener(arg => { this._webSockets = arg })
+        connectedWebSocket.addListener(arg => {
+            arg.send(JSON.stringify({ channel: this._channel, value: this._value }))
+        })
+    }
+    assign(arg) {
+        this._value = arg
+        if (this._webSockets) {
+            this._webSockets.forEach(webSocket => {
+                webSocket.send(JSON.stringify({ channel: this._channel, value: this._value }))
+            })
+        }
+        super.assign(arg)
+    }
+}
