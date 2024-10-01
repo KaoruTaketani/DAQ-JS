aaa0feecf8cdc6acc75bf24fa15103e79bd02b3f
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-10-01 09:22:02 +0900

    store random number in 03

--- 03/index.js
+++ 03/index.js
@@ -1,11 +1,12 @@
 import { Server } from 'http'
 
+const randomNumber = Math.random()
 const httpServer = new Server()
 
 httpServer.on('request', (_, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' })
   response.end(['<html>', '<head>', '<meta charset="utf-8">', '</head>',
-    '<body>', `<p>random number is ${Math.random()}</p>`, '</body>', '</html>'
+    '<body>', `<p>random number is ${randomNumber}</p>`, '</body>', '</html>'
   ].join('\n'))
 })
 httpServer.listen(80)
