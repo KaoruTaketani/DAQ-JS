a7ddd63b2edabb5ae8cc5558135b888673924e9e
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-10-01 09:49:34 +0900

    use ws in 04

--- 04/index.js
+++ 04/index.js
@@ -1,31 +1,61 @@
 import { Server } from 'http'
-import ListenableString from './ListenableString.js'
+import { WebSocketServer } from 'ws'
 
-const httpRequestUrl = new ListenableString()
-let httpResponse
-httpRequestUrl.addListener(arg => {
-    httpResponse.writeHead(200, { 'Content-Type': 'text/html' })
-    httpResponse.end([
+let randomNumber
+const httpServer = new Server()
+const webSocketServer = new WebSocketServer({ noServer: true })
+
+httpServer.on('request', (_, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    response.end([
         '<html>',
-        '',
         '<head>',
         '    <meta charset="utf-8">',
         '</head>',
-        '',
         '<body>',
-        '    <h2>Random Number Generator</h2>',
-        `    <p>random number is ${Math.random()}</p>`,
+        `    <p id="randomNumber">random number is NaN</p>`,
+        `    <script type="module">`,
+        '        const randomNumberElement = document.getElementById("randomNumber")',
+        '        ',
+        '        const socket = new WebSocket("ws://localhost")',
+        '        socket.onclose = () => {',
+        '            document.body.innerHTML = "the connection was closed by the server."',
+        '        }',
+        '        socket.onmessage = event => {',
+        '            const msg = JSON.parse(event.data)',
+        '            console.log(msg)',
+        '            ',
+        '            if (msg.key === "randomNumberInnerText") {',
+        '                randomNumberElement.innerText = msg.value',
+        '            }',
+        '        }',
+        `    </script>`,
         '</body>',
-        '',
         '</html>'
     ].join('\n'))
 })
+httpServer.on('upgrade', (request, socket, head) => {
+    webSocketServer.handleUpgrade(request, socket, head, ws => {
+        // variables.newWebSocket.assign(ws)
+        // ws.on('message', data => {
+        //     const msg = JSON.parse(data)
+        //     console.log(msg)
 
-
-const httpServer = new Server()
-
-httpServer.on('request', (request, response) => {
-    httpResponse = response
-    httpRequestUrl.assign(request.url)
+        //     if (msg.channel === 'clickedElementValue'
+        //         && msg.value === 'generate') {
+        //         ws.send(JSON.stringify({ channel: 'messageInnerText', value: `random number is ${Math.random()}` }))
+        //     }
+        // })
+    })
 })
 httpServer.listen(80)
+
+setInterval(() => {
+    randomNumber = Math.random()
+    webSocketServer.clients.forEach(ws => {
+        ws.send(JSON.stringify({
+            key:'randomNumberInnerText',
+            value:`random number is ${randomNumber}`
+        }))
+    })
+}, 1000)
