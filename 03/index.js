0ac33dc62460803e8bab96c08724c0db86280f3b
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-10-01 09:13:07 +0900

    modified response format in 03

--- 03/index.js
+++ 03/index.js
@@ -2,20 +2,10 @@ import { Server } from 'http'
 
 const httpServer = new Server()
 
-httpServer.on('request', (request, response) => {
+httpServer.on('request', (_, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' })
-  response.end([
-    '<html>',
-    '',
-    '<head>',
-    '    <meta charset="utf-8">',
-    '</head>',
-    '',
-    '<body>',
-    `    <p>random number is ${Math.random()}</p>`,
-    '</body>',
-    '',
-    '</html>'
+  response.end(['<html>', '<head>', '<meta charset="utf-8">', '</head>',
+    '<body>', `<p>random number is ${Math.random()}</p>`, '</body>', '</html>'
   ].join('\n'))
 })
 httpServer.listen(80)
