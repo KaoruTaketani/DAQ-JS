602215dbd1de62c7df33fef2e570201900c5a1e1
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-10-03 10:46:05 +0900

    cleanup element variables from 10

--- 10/RandomNumberInnerTextChanger.js
+++ 10/RandomNumberInnerTextChanger.js
@@ -6,13 +6,20 @@ export default class extends Operator {
      */
     constructor(variables) {
         super()
+        this._webSocketServer
+        variables.webSocketServer.addListener(arg => { this._webSocketServer = arg })
         this._randomNumber
         variables.randomNumber.addListener(arg => {
             this._randomNumber = arg
             this._operation()
         })
         this._operation = () => {
-            variables.randomNumberInnerText.assign(`random number is ${this._randomNumber}`)
+            this._webSocketServer.clients.forEach(ws => {
+                ws.send(JSON.stringify({
+                    key: 'randomNumberInnerText',
+                    value: `random number is ${this._randomNumber}`
+                }))
+            })
         }
     }
 }
--- 10/Variables.js
+++ 10/Variables.js
@@ -1,6 +1,4 @@
 import ControllableBoolean from './ControllableBoolean.js'
-import ElementBoolean from './ElementBoolean.js'
-import ElementString from './ElementString.js'
 import ListenableNumber from './ListenableNumber.js'
 import ListenableObject from './ListenableObject.js'
 
@@ -13,11 +11,6 @@ export default class {
         this.randomNumber = new ListenableNumber()
 
         this.randomNumberGeneratorIsBusy = new ControllableBoolean('randomNumberGeneratorIsBusy', this.message)
-
-        this.randomNumberStopDisabled = new ElementBoolean('randomNumberStopDisabled', this.webSocketServer)
-        this.randomNumberStartDisabled = new ElementBoolean('randomNumberStartDisabled', this.webSocketServer)
-
-        this.randomNumberInnerText = new ElementString('randomNumberInnerText', this.webSocketServer)
     }
 }
 
