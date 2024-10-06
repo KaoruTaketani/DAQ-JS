eae79755c25d3085cc658ed685b91f8cc0a076f3
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-10-07 08:25:12 +0900

    added worker in neunet

---
+++ neunet/worker.js
@@ -0,0 +1,37 @@
+import { Socket } from 'net'
+import { parentPort } from 'worker_threads'
+
+let eventLength
+let eventData = []
+let totalLength = 0
+
+const socket = new Socket()
+socket.on('data', chunk => {
+    if (eventData.length === 0) {
+        eventLength = (chunk.readUint8(2) << 8) + chunk.readUint8(3)
+        eventData.push(chunk.slice(4))
+    } else {
+        eventData.push(chunk)
+    }
+    totalLength += chunk.length
+
+    if (totalLength === eventLength * 2 + 4) {
+        const toParent = Buffer.concat(eventData)
+        parentPort.postMessage(toParent, [toParent.buffer])
+        
+        totalLength = 0
+        eventData = []
+        if (!socket.closed)
+            socket.write(Buffer.from([0xa3, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00]))
+    }
+})
+
+parentPort.on('message', message => {
+    if (message) {
+        socket.connect(port, address, () => {
+            socket.write(Buffer.from([0xa3, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00]))
+        })
+    } else {
+        socket.end()
+    }
+})
