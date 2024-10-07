d2d1cb550d558485b96a1d33283fb078abf1bbe2
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-10-07 10:14:22 +0900

    added influxdb scratch

---
+++ influxdb/write.js
@@ -0,0 +1,57 @@
+import { request } from 'https'
+import token from './token.js'
+import org from './org.js'
+
+const host = 'us-east-1-1.aws.cloud2.influxdata.com'
+
+const req = request(
+    {
+        host: host,
+        path: `/api/v2/write?org=${org()}&bucket=get-started&precision=s`,
+        method: 'POST',
+        headers: {
+            'Authorization': `Token ${token()}`,
+            'Content-Type': 'text/plain; charset=utf-8',
+            'Accept': 'application/json'
+        }
+    },
+    res => {
+        console.log(`STATUS: ${res.statusCode}`)
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
+        res.setEncoding('utf8')
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        })
+        res.on('end', () => {
+            console.log('No more data in response.');
+        })
+    }
+)
+// req.write([
+//     // 'home,room=Living\ Room temp=21.1,hum=35.9,co=0i',
+//     // 'home,room=Kitchen temp=21.0,hum=35.9,co=0i'
+//     `home,room=Kitchen temp=21.0,hum=35.9,co=${Math.floor(Math.random() * 10)}i`
+// ].join('\n'))
+// req.write([
+//     'home,room=LivingRoom temp=21.1,hum=35.9,co=0i'
+// ].join('\n'))
+
+// req.write([
+//     'home,room=Living\\ Room temp=21.1,hum=35.9,co=0i'
+// ].join('\n'))
+
+// req.write([
+//     'home,room=Living\\ Room temp=21.1,hum=35.9,co=0i',
+//     'home,room=Kitchen temp=21.0,hum=35.9,co=0i'
+// ].join('\n'))
+// req.write([
+//     'home,room=Living\\ Room temp=21.4,hum=35.9,co=0i',
+//     'home,room=Kitchen temp=23.0,hum=36.2,co=0i'
+// ].join('\n'))
+req.write([
+    'home,room=Living\\ Room temp=21.8,hum=36.0,co=0i',
+    'home,room=Kitchen temp=22.7,hum=36.1,co=0i'
+].join('\n'))
+
+
+req.end()
