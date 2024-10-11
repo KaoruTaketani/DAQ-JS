091443d44465553f0f03656d04f8e38a0906f70d
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-10-11 16:01:45 +0900

    modified to write to influxdb in edr/write_series.js

--- influxdb/write_series.js
+++ influxdb/write_series.js
@@ -1,4 +1,34 @@
-[
+import { request } from 'https'
+import token from './token.js'
+import org from './org.js'
+
+const req = request(
+    {
+        host: 'us-east-1-1.aws.cloud2.influxdata.com',
+        path: `/api/v2/write?org=${org()}&bucket=get-started&precision=s`,
+        method: 'POST',
+        headers: {
+            'Authorization': `Token ${token()}`,
+            'Content-Type': 'text/plain; charset=utf-8',
+            'Accept': 'application/json'
+        }
+    },
+    res => {
+        console.log(`STATUS: ${res.statusCode}`)
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
+        res.setEncoding('utf8')
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        })
+        res.on('end', () => {
+            console.log('No more data in response.');
+        })
+    }
+)
+
+
+
+const data=[
     'home,room=Living\\ Room temp=21.1,hum=35.9,co=0i',
     'home,room=Kitchen temp=21.0,hum=35.9,co=0i',
     'home,room=Living\\ Room temp=21.4,hum=35.9,co=0i',
@@ -25,13 +55,17 @@
     'home,room=Kitchen temp=23.1,hum=36.6,co=22i',
     'home,room=Living\\ Room temp=22.2,hum=36.4,co=17i',
     'home,room=Kitchen temp=22.7,hum=36.5,co=26i'
-].reduce((previous, value) => previous.then(() =>
+]
+
+data.reduce((previous, value) => previous.then(() =>
     new Promise(resolve => {
         setTimeout(() => {
             console.log(value)
+            req.write(`${value}\n`)
             resolve()
         }, 1000)
     })
 ), Promise.resolve()).then(()=>{
     console.log('end')
+    req.end()
 })
