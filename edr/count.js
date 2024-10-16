a85a37ad31c3165dfcb56deb2b76b2ca14d75227
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-10-16 13:29:25 +0900

    modified edr/count.js

--- edr/count.js
+++ edr/count.js
@@ -8,13 +8,28 @@ let a = 0,
     d = 0
 performance.mark('start')
 createReadStream(filePath()).on('data', chunk => {
-    for (let i = 0; i < chunk.length; ++i) {
-        if (chunk[8 * i] === 0x5b) b++
+    for (let i = 0; i < chunk.length / 8; ++i) {
+        switch (chunk[8 * i]) {
+            case 0x5a:
+                a++
+                break
+            case 0x5b:
+                b++
+                break
+            case 0x5c:
+                c++
+                break
+            default:
+                d++
+        }
     }
 }).on('end', () => {
     performance.mark('8bit')
-    console.log(`b: ${b.toLocaleString()}, ${performance.measure('', 'start', '8bit').duration} ms`)
+    console.log(`a: ${a.toLocaleString()}, b: ${b.toLocaleString()}, c: ${c.toLocaleString()}, d: ${d.toLocaleString()}, ${performance.measure('', 'start', '8bit').duration} ms`)
+    a = 0
     b = 0
+    c = 0
+    d = 0
     createReadStream(filePath()).on('data', chunk => {
         const array = new BigUint64Array(
             chunk.buffer,
@@ -23,10 +38,16 @@ createReadStream(filePath()).on('data', chunk => {
         for (let i = 0; i < array.length; ++i) {
             if (array[i] & 0b1n) {
                 b++
+            } else {
+                if (array[i] & 0b10n) {
+                    a++
+                } else {
+                    c++
+                }
             }
         }
     }).on('close', () => {
         performance.mark('64bit')
-        console.log(`b: ${b.toLocaleString()}, ${performance.measure('', '8bit', '64bit').duration} ms`)
+        console.log(`a: ${a.toLocaleString()}, b: ${b.toLocaleString()}, c: ${c.toLocaleString()}, d: ${d.toLocaleString()}, ${performance.measure('', '8bit', '64bit').duration} ms`)
     })
 })
