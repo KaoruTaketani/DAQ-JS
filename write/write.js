1a8e15f4abe8467ab8cef46ac642bafb1f5e6833
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-10-18 14:35:02 +0900

    added drain count in write.js

--- write/write.js
+++ write/write.js
@@ -1,25 +1,27 @@
 import { createWriteStream, statSync } from 'fs'
 
 const
-    // totalLength = 1024,
+    totalLength = 1024,
     // totalLength = 64 * 1024,
-    totalLength = 128 * 1024,
-    // chunkLength = 128,
-    chunkLength = 1024,
+    // totalLength = 128 * 1024,
+    chunkLength = 128,
+    // chunkLength = 1024,
     data = new Float64Array(totalLength)
 let offset = 0
+let drainCount = 0
 for (let i = 0; i < totalLength; ++i) data[i] = Math.random()
 
 const s = createWriteStream('../../tmp', {
     encoding: 'binary'
 }).on('drain', () => {
     console.log('drain')
+    drainCount++
     interval = setInterval(f)
 }).on('end', () => {
     console.log('end')
 }).on('finish', () => {
     console.log('finish')
-    console.log(`${statSync('../../tmp').size}, ${8 * totalLength} bytes, elapsedTime: ${Date.now() - startTime} ms, rate: ${1000 * 8 * totalLength / (Date.now() - startTime) / 1024 / 1024} MB/s`)
+    console.log(`${statSync('../../tmp').size}, ${8 * totalLength} bytes, drainCount: ${drainCount}, elapsedTime: ${Date.now() - startTime} ms, rate: ${1000 * 8 * totalLength / (Date.now() - startTime) / 1024 / 1024} MB/s`)
     clearInterval(interval)
 }).on('close', () => {
     console.log('close')
