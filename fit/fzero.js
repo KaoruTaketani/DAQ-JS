e4c13e81e79b878de4eeabba31b20ba64346b1cf
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-11-05 13:18:38 +0900

    added fzero

---
+++ fit/fzero.js
@@ -0,0 +1,25 @@
+import sub2ind from './sub2ind.js'
+import eye from './eye.js'
+
+export default (
+    fname,
+    x0
+) => {
+    let f, df, x1
+    if (fname === 'sin') {
+        f = x => Math.sin(x)
+        df = x => Math.cos(x)
+    }
+    if (fname === 'cos') {
+        f = x => Math.cos(x)
+        df = x => -Math.sin(x)
+    }
+    if (f === undefined) return
+    /// @NewtonRaphsonMethod
+    for (let i = 0; i < 10; ++i) {
+        x1 = x0 - f(x0) / df(x0)
+        if (Math.abs(x1 - x0) < 1e-4) break
+        x0 = x1
+    }
+    return x0
+}
---
+++ fit/fzero1.js
@@ -0,0 +1,5 @@
+import fzero from './fzero.js'
+
+console.log(`${fzero('sin', 3.0)}`)
+console.log(`${fzero('cos', 1.5)}`)
+
