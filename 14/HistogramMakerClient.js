6dffd1699d4f513257fffb7d4fe2627eca058a09
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-11-14 13:25:57 +0900

    modified element variables in 14

--- 14/HistogramMakerClient.js
+++ 14/HistogramMakerClient.js
@@ -1,9 +1,7 @@
 const totalElement = document.createElement('p')
-totalElement.innerText = 'total is NaN'
 document.body.appendChild(totalElement)
 
 const startTimeElement = document.createElement('p')
-startTimeElement.innerText = 'start time is undefined'
 document.body.appendChild(startTimeElement)
 
 const url = new URL(import.meta.url)
@@ -13,10 +11,10 @@ socket.onclose = () => {
     document.body.innerHTML = "the connection was closed by the server."
 }
 socket.onmessage = event => {
-    const msg = JSON.parse(event.data)
+    const elementValue = JSON.parse(event.data)
 
-    if (msg.key === 'totalInnerText')
-        totalElement.innerText = msg.value
-    if (msg.key === 'startTimeInnerText')
-        startTimeElement.innerText = msg.value
+    if (elementValue.hash === '#totalInnerText')
+        totalElement.innerText = elementValue.value
+    if (elementValue.hash === '#startTimeInnerText')
+        startTimeElement.innerText = elementValue.value
 }
--- 14/index.js
+++ 14/index.js
@@ -1,28 +1,32 @@
 import { Server } from 'http'
 import HistogramInitializer from './HistogramInitializer.js'
 import HistogramMaker from './HistogramMaker.js'
-import TotalInnerTextChanger from './TotalInnerTextChanger.js'
-import TotalCounter from './TotalCounter.js'
+import HistogramMakerInitializer from './HistogramMakerInitializer.js'
 import HTTPServerRequestHandler from './HTTPServerRequestHandler.js'
 import HTTPServerSetupper from './HTTPServerSetupper.js'
 import HTTPServerUpgradeHandler from './HTTPServerUpgradeHandler.js'
 import RandomNumberGenerator from './RandomNumberGenerator.js'
+import RandomNumberGeneratorInitializer from './RandomNumberGeneratorInitializer.js'
 import RandomNumberInnerTextChanger from './RandomNumberInnerTextChanger.js'
 import RandomNumberStartDisabledChanger from './RandomNumberStartDisabledChanger.js'
 import RandomNumberStopDisabledChanger from './RandomNumberStopDisabledChanger.js'
-import Variables from './Variables.js'
 import StartTimeInnerTextChanger from './StartTimeInnerTextChanger.js'
+import TotalCounter from './TotalCounter.js'
+import TotalInnerTextChanger from './TotalInnerTextChanger.js'
+import Variables from './Variables.js'
 
 const variables = new Variables()
 
 new HistogramInitializer(variables)
 new HistogramMaker(variables)
+new HistogramMakerInitializer(variables)
 new TotalInnerTextChanger(variables)
 new TotalCounter(variables)
 new HTTPServerRequestHandler(variables)
 new HTTPServerSetupper(variables)
 new HTTPServerUpgradeHandler(variables)
 new RandomNumberGenerator(variables)
+new RandomNumberGeneratorInitializer(variables)
 new RandomNumberInnerTextChanger(variables)
 new RandomNumberStartDisabledChanger(variables)
 new RandomNumberStopDisabledChanger(variables)
