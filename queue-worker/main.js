0b89a0b8fb07d1f388fbdaccf08c77c4b1d7016e
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-11-24 15:28:01 +0900

    added scratch in queue-worker

---
+++ queue-worker/main.js
@@ -0,0 +1,15 @@
+import { Worker } from 'worker_threads'
+
+// const worker = new Worker('./worker.js', { stdout: true })
+// worker.stdout.on('data',chunk=>{
+//     console.log(chunk.toString().trim())
+// })
+const worker = new Worker('./worker.js')
+worker.on('message', data => {
+    console.log(`data.length: ${data.length.toLocaleString()}, heapUsed: ${process.memoryUsage().heapUsed.toLocaleString()}, arrayBuffers: ${process.memoryUsage().arrayBuffers.toLocaleString()}, rss: ${process.memoryUsage().rss.toLocaleString()}`)
+    for (let i = 0; i < 1e9; ++i) { }
+})
+
+worker.on('exit', code => {
+    console.log(`worker exit code: ${code}`)
+})
