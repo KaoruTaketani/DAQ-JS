ae1460ce5ed8ea2c8af9c078417c6d5c25ea44a2
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-12-01 17:21:08 +0900

    modified scratch of loop in lsqcurvefit

--- fit/lsqcurvefit.js
+++ fit/lsqcurvefit.js
@@ -40,31 +40,73 @@ export default (
     // console.log(ydata[0] - f[0](xdata[0], p0))
     // console.log(f[1](xdata[0], p0))
     const J = zeros(n, p0.length),
-        dy = zeros(n, 1)
+        dy = zeros(n, 1),
+        p = new Array(p0.length)
+    for (let i = 0; i < p0.length; ++i) {
+        p[i] = p0[i]
+    }
+
     for (let i = 0; i < n; ++i) {
-        dy[i][0] = ydata[i] - f[0](xdata[i], p0)
-        J[i] = f[1](xdata[i], p0)
+        dy[i][0] = ydata[i] - f[0](xdata[i], p)
+        J[i] = f[1](xdata[i], p)
     }
     // console.log(dy)
     // console.log(J)
     let h = mldivide(mtimes(transpose(J), J), mtimes(transpose(J), dy))
-    let p = zeros(p0.length, 1)
+    // let p = zeros(p0.length, 1)
+    // for (let i = 0; i < p0.length; ++i) {
+    //     p[i][0] = p0[i]
+    // }
+    console.log(p)
+    // console.log(h)
     for (let i = 0; i < p0.length; ++i) {
-        p[i][0] = p0[i]
+        p[i] += h[i][0]
     }
     console.log(p)
-    console.log(h)
-    p = plus(p, h)
-    console.log(p)
-    console.log(transpose(p))
+
+    /// 1
     for (let i = 0; i < n; ++i) {
-        dy[i][0] = ydata[i] - f[0](xdata[i], transpose(p)[0])
-        J[i] = f[1](xdata[i], transpose(p)[0])
+        dy[i][0] = ydata[i] - f[0](xdata[i], p)
+        J[i] = f[1](xdata[i], p)
     }
     h = mldivide(mtimes(transpose(J), J), mtimes(transpose(J), dy))
-    console.log(h)
-    p = plus(p, h)
+    for (let i = 0; i < p0.length; ++i) {
+        p[i] += h[i][0]
+    }
     console.log(p)
+
+    /// 2
+    for (let i = 0; i < n; ++i) {
+        dy[i][0] = ydata[i] - f[0](xdata[i], p)
+        J[i] = f[1](xdata[i], p)
+    }
+    h = mldivide(mtimes(transpose(J), J), mtimes(transpose(J), dy))
+    for (let i = 0; i < p0.length; ++i) {
+        p[i] += h[i][0]
+    }
+    console.log(p)
+
+    /// 3
+    for (let i = 0; i < n; ++i) {
+        dy[i][0] = ydata[i] - f[0](xdata[i], p)
+        J[i] = f[1](xdata[i], p)
+    }
+    h = mldivide(mtimes(transpose(J), J), mtimes(transpose(J), dy))
+    for (let i = 0; i < p0.length; ++i) {
+        p[i] += h[i][0]
+    }
+    console.log(p)
+    // p = plus(p, h)
+    // console.log(p)
+    // console.log(transpose(p))
+    // for (let i = 0; i < n; ++i) {
+    //     dy[i][0] = ydata[i] - f[0](xdata[i], transpose(p)[0])
+    //     J[i] = f[1](xdata[i], transpose(p)[0])
+    // }
+    // h = mldivide(mtimes(transpose(J), J), mtimes(transpose(J), dy))
+    // console.log(h)
+    // p = plus(p, h)
+    // console.log(p)
 }
 
 
