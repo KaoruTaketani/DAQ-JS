d23a2bfa8a4230d15b2c24699647546d687c8843
Kaoru Taketani <kaoru.taketani@gmail.com>
2024-12-06 09:00:29 +0900

    copied missings from 18 to 20

---
+++ 20/TimeSeriesMakerClient.js
@@ -0,0 +1,19 @@
+const url = new URL(import.meta.url)
+url.protocol = 'ws:'
+url.pathname = ''
+const socket = new WebSocket(url)
+socket.onclose = () => {
+    document.body.innerHTML = "the connection was closed by the server."
+}
+
+const timeSeriesSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+timeSeriesSVGElement.setAttribute('width', '400')
+timeSeriesSVGElement.setAttribute('height', '300')
+timeSeriesSVGElement.setAttribute('viewBox', '0 0 560 420')
+url.pathname = 'timeSeriesSVGInnerHTML'
+const timeSeriesSVGInnerHTMLSocket = new WebSocket(url)
+timeSeriesSVGInnerHTMLSocket.onmessage = event => {
+    timeSeriesSVGElement.innerHTML = event.data
+}
+document.body.appendChild(timeSeriesSVGElement)
+
--- 20/Variables.js
+++ 20/Variables.js
@@ -3,13 +3,16 @@ import ElementBoolean from './ElementBoolean.js'
 import ElementString from './ElementString.js'
 import ListenableNumber from './ListenableNumber.js'
 import ListenableObject from './ListenableObject.js'
+import WritableHistogram from './WritableHistogram.js'
 
 export default class {
     constructor() {
         this.httpServer = new ListenableObject()
         this.message = new ListenableObject()
         this.webSocketPathnames = new ListenableObject()
-        this.histogram = new ListenableObject()
+        this.timeSeries = new ListenableObject()
+        this.histogramHDF5File = new ListenableObject()
+        this.histogram = new WritableHistogram('rand', this.histogramHDF5File)
         this.elementValues = new ListenableObject()
 
         this.randomNumber = new ListenableNumber()
@@ -20,12 +23,13 @@ export default class {
 
         this.stopButtonDisabled = new ElementBoolean('/stopButtonDisabled', this.elementValues, this.webSocketPathnames)
         this.startButtonDisabled = new ElementBoolean('/startButtonDisabled', this.elementValues, this.webSocketPathnames)
-
         this.randomNumberInnerText = new ElementString('/randomNumberInnerText', this.elementValues, this.webSocketPathnames)
 
         this.totalInnerText = new ElementString('/totalInnerText', this.elementValues, this.webSocketPathnames)
         this.startTimeInnerText = new ElementString('/startTimeInnerText', this.elementValues, this.webSocketPathnames)
         this.histogramSVGInnerHTML = new ElementString('/histogramSVGInnerHTML', this.elementValues, this.webSocketPathnames)
+
+        this.timeSeriesSVGInnerHTML = new ElementString('/timeSeriesSVGInnerHTML', this.elementValues, this.webSocketPathnames)
     }
 }
 
