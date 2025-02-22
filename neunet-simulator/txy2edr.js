851e58acbfe7561d188b2a4ac22a9522e7fb9e12
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-02-23 05:49:15 +0900

    refactored simulator

--- neunet-simulator/txy2edr.js
+++ neunet-simulator/txy2edr.js
@@ -23,11 +23,11 @@ export default (time, x, y) => {
     if (time / 25 > 2 ** 24) {
         time = 2 ** 24 / 25 - 1
     }
-    buffer.writeUint8((time / 25) >> 16, 1)
-    buffer.writeUint8((time / 25 & 0x00ff00) >> 8, 2)
-    buffer.writeUint8((time / 25 & 0x0000ff), 3)
+    buffer[1] = (time / 25) >> 16
+    buffer[2] = (time / 25 & 0x00ff00) >> 8
+    buffer[3] = (time / 25 & 0x0000ff)
     const channel0 = 0
-    buffer.writeUint8(channel0, 4)
+    buffer[4] = channel0
     const total = 1800
     let left0 = total * x
     let right0 = total * (1 - x)
@@ -40,23 +40,16 @@ export default (time, x, y) => {
         right0 = 0
     }
     // mask w/ 0xf extracts the corresponding 4bit, 2^4=16, 
-    // and shift the result to fill uint8, 2^8=256
-    const i5 = (left0 & 0xff0) >> 4
-    const i6 = ((left0 & 0x00f) << 4) + ((right0 & 0xf00) >> 8)
-    const i7 = right0 & 0x0ff
-    buffer.writeUint8(i5, 5)
-    buffer.writeUint8(i6, 6)
-    buffer.writeUint8(i7, 7)
-    // console.log(`${left0} ${(i5 << 4) + (i6 >> 4)} ${right0} ${((i6 & 0xf) << 8) + i7}`)
-    // left = (i5 << 4) + (i6 >> 4),
-    // right = ((i6 & 0xf) << 8) + i7
+    buffer[5] = (left0 & 0xff0) >> 4
+    buffer[6] = ((left0 & 0x00f) << 4) + ((right0 & 0xf00) >> 8)
+    buffer[7] = right0 & 0x0ff
 
 
-    buffer.writeUint8((time / 25) >> 16, 9)
-    buffer.writeUint8((time / 25 & 0x00ff00) >> 8, 10)
-    buffer.writeUint8((time / 25 & 0x0000ff), 11)
+    buffer[9] = (time / 25) >> 16
+    buffer[10] = (time / 25 & 0x00ff00) >> 8
+    buffer[11] = (time / 25 & 0x0000ff)
     const channel1 = 1
-    buffer.writeUint8(channel1, 12)
+    buffer[12] = channel1
     let left1 = total * y
     let right1 = total * (1 - y)
     if (y < 0) {
@@ -67,15 +60,9 @@ export default (time, x, y) => {
         left1 = total
         right1 = 0
     }
-    const i13 = (left1 & 0xff0) >> 4
-    const i14 = ((left1 & 0x00f) << 4) + ((right1 & 0xf00) >> 8)
-    const i15 = right1 & 0x0ff
-    buffer.writeUint8(i13, 13)
-    buffer.writeUint8(i14, 14)
-    buffer.writeUint8(i15, 15)
-    // console.log(`${left1} ${(i13 << 4) + (i14 >> 4)} ${right1} ${((i14 & 0xf) << 8) + i15}`)
-    // console.log(`x: ${x},${Math.floor(1024 * left0 / (left0 + right0))} y: ${y},${Math.floor(1024 * left1 / (left1 + right1))} `)
-    // x: Math.floor(1024 * xLeft / (xLeft + xRight)),
-    // y: Math.floor(1024 * yLeft / (yLeft + yRight)),
+    buffer[13] = (left1 & 0xff0) >> 4
+    buffer[14] = ((left1 & 0x00f) << 4) + ((right1 & 0xf00) >> 8)
+    buffer[15] = right1 & 0x0ff
+
     return buffer
 }
