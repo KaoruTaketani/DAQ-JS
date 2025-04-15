6cbf51be98c827b68ea911c7b9d0eaac1ea6144b
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-04-15 14:56:12 +0900

    implemented attr table inner html maker

--- offline-viewer/AttrTableInnerHTMLMaker.js
+++ offline-viewer/AttrTableInnerHTMLMaker.js
@@ -17,24 +17,52 @@ export default class extends Operator {
             this._operation()
         })
         this._operation = () => {
-            this._attrMap = new Map()
             readdir(this._hdf5Path, (err, files) => {
                 if (err) throw err
 
+                /** @type {object[]} */
+                const metadata = []
                 files.forEach(file => {
+                    const tmp = new Map()
+                    tmp.set('name', file)
                     const f = new h5wasm.File(join(this._hdf5Path, file), 'r')
                     Object.keys(f.attrs).forEach(key => {
-                        this._attrMap.set(key, f.attrs[key].value)
+                        tmp.set(key, f.attrs[key].value)
                     })
                     f.close()
+                    metadata.push(Object.fromEntries(tmp))
                 })
-                console.log('attr done')
-                variables.attrTableInnerHTML.assign(['<html>',
+                console.log('metadata done')
+                const keys = new Set()
+                metadata.forEach(row => {
+                    Object.keys(row).forEach(key => {
+                        keys.add(key)
+                    })
+                })
+                const headerInnerHTML = '<tr>' + Array.from(keys).map(key => {
+                    return `<th>${key}</th>`
+                }).join('') + '</tr>'
+                const bodyInnerHTML = metadata.map(row => {
+                    return '<tr>' + Array.from(keys).map(key => {
+                        const cellMap = new Map(Object.entries(row))
+                        return `<td>${cellMap.get(key)}</td>`
+                    }).join('') + '</tr>'
+                }).join('')
+                console.log('table done')
+                variables.attrTableInnerHTML.assign([
+                    '<html>',
                     '<head>',
                     '    <meta charset="utf-8">',
                     '</head>',
                     '<body>',
-                    '    <p>attr table</p>',
+                    '    <table>',
+                    '        <thead>',
+                    headerInnerHTML,
+                    '        </thead>',
+                    '        <tbody>',
+                    bodyInnerHTML,
+                    '        </tbody>',
+                    '    </table>',
                     '</body>',
                     '</html>'
                 ].join('\n'))
