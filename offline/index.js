95eef5873adf4ac04b0fca7bfe7be36c8c74d926
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-04-16 18:15:29 +0900

    added json files with different freq vec length

--- offline/index.js
+++ offline/index.js
@@ -58,7 +58,16 @@ new MomentumTransferMaker(variables)
 new JSONFileReader(variables)
 
 variables.hdf5Path.assign('../../hdf5/mieze')
-variables.jsonFilePaths.assign(['./104.json', './106.json', './113.json', './115.json'])
+variables.jsonFilePaths.assign([
+    './104.json',
+    './104_16.json',
+    './104_32.json',
+    './106.json',
+    './106_16.json',
+    './106_32.json',
+    './113.json',
+    './115.json'
+])
 // variables.jsonFilePaths.assign(['./104.json'])
 // variables.jsonFilePaths.assign(['./106.json'])
 
