2518393c54995871ddfe224537b64d862f2e1e73
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-04-30 08:36:14 +0900

    modified block list

--- block/index.js
+++ block/index.js
@@ -2,14 +2,15 @@ import { Server } from 'http'
 import { BlockList } from 'net'
 
 const blockList = new BlockList()
-// blockList.addAddress('192.168.1.100')
+
 blockList.addRange('0.0.0.0', '255.255.255.255')
 
 const server = new Server()
 server.on('request', (req, res) => {
     const remoteAddress = req.socket.remoteAddress
-    console.log(`remoteAddress: ${remoteAddress}`)
-    if (blockList.check(remoteAddress)) {
+
+    if (blockList.check(remoteAddress)
+        || blockList.check(remoteAddress, 'ipv6')) {
         res.statusCode = 403
         res.end('Forbidden')
     } else {
