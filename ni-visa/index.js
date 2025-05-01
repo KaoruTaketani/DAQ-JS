92b9a169f62c244faa61eaf2e0662ef6ddeba184
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-05-02 08:42:07 +0900

    modified ni-visa

--- ni-visa/index.js
+++ ni-visa/index.js
@@ -1,1 +1,14 @@
-console.log('ni-visa')
+import * as NiVisa from 'node-ni-visa'
+
+const driverSession = NiVisa.viOpenDefaultRM()
+const deviceSession = NiVisa.viOpen(driverSession, 'USB0::0x0D4A::0x000E::9139964::INSTR')
+
+console.log('Write result:', NiVisa.viWrite(deviceSession, '*IDN?\n'))
+console.log('Read result:', NiVisa.viRead(deviceSession))
+
+const queryMessage = '*IDN?\n'
+console.log(`Query '${queryMessage}' -> '${NiVisa.query(deviceSession, queryMessage)}'`)
+
+NiVisa.viClose(deviceSession)
+NiVisa.viClose(driverSession)
+
