dd66edd4b7439d3dfed734a01a83970e84180f05
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-06-04 15:48:00 +0900

    changed client's message handler in edr-table

--- edr-table/ChannelEventTableClient.js
+++ edr-table/ChannelEventTableClient.js
@@ -4,11 +4,21 @@ const socket = new WebSocket(url)
 socket.onclose = () => {
     document.body.innerHTML = "the connection was closed by the server."
 }
-/** @type {function[]} */
-const messageListeners = []
+/** @type {Map<string,function>} */
+const listeners = new Map()
+/**
+ * 
+ * @param {string} channel 
+ * @param {function} listener 
+ */
+function on(channel, listener) {
+    listeners.set(channel, listener)
+}
 socket.addEventListener('message', event => {
     const arg = JSON.parse(event.data)
-    messageListeners.forEach(listener => { listener(arg) })
+    for (const [key, value] of Object.entries(arg)) {
+        listeners.get(key)?.(value)
+    }
 })
 
 /** @type {HTMLInputElement} */
@@ -17,12 +27,7 @@ offsetElement.type = 'number'
 offsetElement.style.marginTop = '508px'
 offsetElement.style.position = 'absolute'
 offsetElement.min = '0'
-messageListeners.push((/** @type {object} */arg) => {
-    for (const [key, value] of Object.entries(arg)) {
-        if (key !== 'offsetValue') return
-        offsetElement.value = value
-    }
-})
+on('offsetValue', (/** @type {string} */ arg) => { offsetElement.value = arg })
 offsetElement.onchange = () => {
     const value = parseInt(offsetElement.value)
     if (!Number.isInteger(value)) return
@@ -36,13 +41,7 @@ fileNamesElement.size = 20
 fileNamesElement.style.position = 'absolute'
 fileNamesElement.style.width = '150px'
 fileNamesElement.style.height = `500px`
-messageListeners.push((/** @type {object} */arg) => {
-    for (const [key, value] of Object.entries(arg)) {
-        // console.log(`${key}: ${value}`);
-        if (key !== 'edrFileNamesInnerHTML') return
-        fileNamesElement.innerHTML = value
-    }
-})
+on('edrFileNamesInnerHTML', (/** @type {string} */ arg) => { fileNamesElement.innerHTML = arg })
 fileNamesElement.onchange = () => {
     socket.send(JSON.stringify({ edrReaderFileName: fileNamesElement.selectedOptions[0].innerText }))
 }
@@ -53,22 +52,12 @@ const messageElement = document.createElement('div')
 // messageElement.style.marginTop = '608px'
 // messageElement.style.position = 'absolute'
 messageElement.style.marginLeft = '158px'
-messageListeners.push((/** @type {object} */arg) => {
-    for (const [key, value] of Object.entries(arg)) {
-        if (key !== 'messageInnerText') return
-        messageElement.innerText = value
-    }
-})
+on('messageInnerText', (/** @type {string} */ arg) => { messageElement.innerText = arg })
 document.body.appendChild(messageElement)
 
 /** @type {HTMLTableElement} */
 const tableElement = document.createElement('table')
 tableElement.style.marginLeft = '158px'
-messageListeners.push((/** @type {object} */arg) => {
-    for (const [key, value] of Object.entries(arg)) {
-        if (key !== 'tableInnerHTML') return
-        tableElement.innerHTML = value
-    }
-})
+on('tableInnerHTML', (/** @type {string} */ arg) => { tableElement.innerHTML = arg })
 document.body.appendChild(tableElement)
 
