636550e821f8e3fecd55af87bd8935707762e0d7
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-06-06 10:20:22 +0900

    cleanup server variables from edr-table

--- edr-table/ServerVariables.js
+++
@@ -1,9 +0,0 @@
-import ListenableObject from './ListenableObject.js'
-
-export default class {
-    constructor() {
-        /** @type {import('./ListenableObject.js').default<import('http').Server>} */
-        this.httpServer = new ListenableObject()
-    }
-}
-
--- edr-table/index.js
+++ edr-table/index.js
@@ -1,13 +1,70 @@
-import HTTPRequestHandler from './HTTPRequestHandler.js'
-import HTTPServerSetupper from './HTTPServerSetupper.js'
-import HTTPUpgradeHandler from './HTTPUpgradeHandler.js'
-import ServerVariables from './ServerVariables.js'
+import { readFile } from 'fs'
 import { Server } from 'http'
+import { basename } from 'path'
+import { WebSocketServer } from 'ws'
+import ChannelEventTableServer from './ChannelEventTableServer.js'
+import NeutronEventTableServer from './NeutronEventTableServer.js'
+import PairedEventTableServer from './PairedEventTableServer.js'
 
-const variables = new ServerVariables()
+const httpServer = new Server(),
+    webSocketServer = new WebSocketServer({ noServer: true }),
+    servers = new Map()
 
-new HTTPRequestHandler(variables)
-new HTTPUpgradeHandler(variables)
-new HTTPServerSetupper(variables)
+httpServer.on('request', (request, response) => {
+    if (request.url?.endsWith('.js')) {
+        readFile(`.${request.url}`, 'utf8', (err, data) => {
+            if (err) throw err
 
-variables.httpServer.assign(new Server())
+            response.writeHead(200, { 'Content-Type': 'text/javascript' })
+            response.end(data)
+        })
+    } else if (request.url?.endsWith('.html')) {
+        response.writeHead(200, { 'Content-Type': 'text/html' })
+        response.end([
+            '<html>',
+            '<head>',
+            '    <meta charset="utf-8">',
+            '</head>',
+            '<body>',
+            `    <script type="module" src="./${basename(request.url, '.html')}.js">`,
+            `    </script>`,
+            '</body>',
+            '</html>'
+        ].join('\n'))
+    } else {
+        response.writeHead(200, { 'Content-Type': 'text/html' })
+        response.end([
+            '<html>',
+            '<head>',
+            '    <meta charset="utf-8">',
+            '</head>',
+            '<body>',
+            `    <p><a href="./ChannelEventTableClient.html">Channel</a></p>`,
+            `    <p><a href="./PairedEventTableClient.html">Paired</a></p>`,
+            `    <p><a href="./NeutronEventTableClient.html">Neutron</a></p>`,
+            '</body>',
+            '</html>'
+        ].join('\n'))
+    }
+}).on('upgrade', (request, socket, head) => {
+    webSocketServer.handleUpgrade(request, socket, head, ws => {
+        if (request.url === '/ChannelEventTableClient.js')
+            servers.set(ws, new ChannelEventTableServer(ws))
+        if (request.url === '/PairedEventTableClient.js')
+            servers.set(ws, new PairedEventTableServer(ws))
+        if (request.url === '/NeutronEventTableClient.js')
+            servers.set(ws, new NeutronEventTableServer(ws))
+
+        ws.on('message', data => {
+            // console.log(`onmessage url:${request.url}`)
+
+            const arg = JSON.parse(data.toString())
+            servers.get(ws)?.variables.message.assign(arg)
+        })
+        ws.on('close', () => {
+            console.log(`a ws closed by the client url: ${request.url}`)
+
+            servers.delete(ws)
+        })
+    })
+}).listen(80)
