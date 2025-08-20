dae7d8d545cf74ec8a6a67616133e21729d8139e
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-08-20 09:41:00 +0900

    refactored files in lib

---
+++ lib/index.d.ts
@@ -0,0 +1,42 @@
+export interface XY {
+    x: number[]
+    y: number[]
+}
+
+export interface Histogram {
+    binCounts: number[]
+    binWidth: number
+}
+
+export interface Histogram2D {
+    binCounts: number[]
+    numBins: number[]
+    binWidth: number[]
+}
+
+export interface Histogram3D {
+    binCounts: number[]
+    numBins: number[]
+    binWidth: number[]
+}
+
+export interface Axes {
+    xLim: number[]
+    yLim: number[]
+    zLim?: number[]
+    xTick: number[]
+    yTick: number[]
+    xTickLabel: string[]
+    yTickLabel: string[]
+    parentWidth?: number
+    parentHeight?: number
+    innerLeft?: number
+    innerBottom?: number
+    innerWidth?: number
+    innerHeight?: number
+    xTickLabelGapOffset?: number
+    yTickLabelGapOffset?: number
+    xScale?: string
+    yScale?: string
+    zScale?: string
+}
