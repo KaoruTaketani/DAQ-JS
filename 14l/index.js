40d9c0740dc499b9fc0cf0d558a429177e639bf6
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-08-25 13:58:24 +0900

    added 14l for horizontally restricted histogram plot

---
+++ 14l/index.js
@@ -0,0 +1,30 @@
+import { Server } from 'http'
+import HistogramInitializer from '../14/HistogramInitializer.js'
+import HistogramMaker from '../14/HistogramMaker.js'
+import HistogramSVGInnerHTMLMaker from './HistogramSVGInnerHTMLMaker.js'
+import HTTPGetHandler from '../14/HTTPGetHandler.js'
+import HTTPServerSetupper from '../14/HTTPServerSetupper.js'
+import HTTPUpgradeHandler from '../14/HTTPUpgradeHandler.js'
+import RandomNumberGenerator from '../14/RandomNumberGenerator.js'
+import RandomNumberInnerTextChanger from '../14/RandomNumberInnerTextChanger.js'
+import StartButtonDisabledChanger from '../14/StartButtonDisabledChanger.js'
+import StartTimeInnerTextChanger from '../14/StartTimeInnerTextChanger.js'
+import StopButtonDisabledChanger from '../14/StopButtonDisabledChanger.js'
+import Variables from '../14/Variables.js'
+
+const variables = new Variables()
+
+new HistogramInitializer(variables)
+new HistogramMaker(variables)
+new HistogramSVGInnerHTMLMaker(variables)
+new HTTPGetHandler(variables)
+new HTTPServerSetupper(variables)
+new HTTPUpgradeHandler(variables)
+new RandomNumberGenerator(variables)
+new RandomNumberInnerTextChanger(variables)
+new StartButtonDisabledChanger(variables)
+new StopButtonDisabledChanger(variables)
+new StartTimeInnerTextChanger(variables)
+
+variables.httpServer.assign(new Server()) 
+variables.randomNumberGeneratorIsBusy.assign(false)
