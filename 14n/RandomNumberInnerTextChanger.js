117a97b62f07aa82f66fdf9913769ee460f27845
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-08-28 08:15:01 +0900

    added throttle in randon number inner text changer in 14n

---
+++ 14n/RandomNumberInnerTextChanger.js
@@ -0,0 +1,20 @@
+import Operator from '../14/Operator.js'
+import throttle from './throttle.js'
+
+export default class extends Operator {
+    /**
+     * @param {import('./Variables.js').default} variables 
+     */
+    constructor(variables) {
+        super()
+        this._randomNumber
+        variables.randomNumber.addListener(arg => {
+            this._randomNumber = arg
+            this._operation()
+        })
+        this._operation = throttle(() => {
+            variables.randomNumberInnerText.assign(`random number is ${this._randomNumber}`)
+        }, 200)
+    }
+}
+
