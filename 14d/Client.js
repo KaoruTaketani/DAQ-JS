13b1f92213be252d6f6bbb1ad3e8f6bae6bbbcc3
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-09-02 15:31:38 +0900

    changed formats of Client.js in 14d

--- 14d/Client.js
+++ 14d/Client.js
@@ -6,99 +6,89 @@ socket.onclose = () => {
     document.body.innerHTML = "the connection was closed by the server."
 }
 
-const startButtonElement = document.createElement('input')
-startButtonElement.type = 'button'
-startButtonElement.value = 'start'
-startButtonElement.style.width = '130px'
-startButtonElement.onclick = () => {
-    socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: true }))
-}
-url.pathname = 'startButtonDisabled'
-const startButtonDisabledSocket = new WebSocket(url)
-startButtonDisabledSocket.onmessage = event => {
-    startButtonElement.disabled = event.data === 'true'
-}
-document.body.appendChild(startButtonElement)
+(element => {
+    element.type = 'button'
+    element.value = 'start'
+    element.style.width = '130px'
+    element.onclick = () => {
+        socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: true }))
+    }
+    url.pathname = 'startButtonDisabled'
+    const disabledSocket = new WebSocket(url)
+    disabledSocket.onmessage = event => {
+        element.disabled = event.data === 'true'
+    }
+})(document.body.appendChild(document.createElement('input')));
 
-const stopButtonElement = document.createElement('input')
-stopButtonElement.type = 'button'
-stopButtonElement.value = 'stop'
-stopButtonElement.style.width = '130px'
-stopButtonElement.onclick = () => {
-    socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: false }))
-}
-url.pathname = 'stopButtonDisabled'
-const stopButtonDisabledSocket = new WebSocket(url)
-stopButtonDisabledSocket.onmessage = event => {
-    stopButtonElement.disabled = event.data === 'true'
-}
-document.body.appendChild(stopButtonElement)
+(element => {
+    element.type = 'button'
+    element.value = 'stop'
+    element.style.width = '130px'
+    element.onclick = () => {
+        socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: false }))
+    }
+    url.pathname = 'stopButtonDisabled'
+    const disabledSocket = new WebSocket(url)
+    disabledSocket.onmessage = event => {
+        element.disabled = event.data === 'true'
+    }
+})(document.body.appendChild(document.createElement('input')));
 
-const startTimeElement = document.createElement('p')
-url.pathname = 'startTimeInnerText'
-const startTimeInnerTextSocket = new WebSocket(url)
-startTimeInnerTextSocket.onmessage = event => {
-    startTimeElement.innerText = event.data
-}
-document.body.appendChild(startTimeElement)
+(element => {
+    url.pathname = 'startTimeInnerText'
+    const innerTextSocket = new WebSocket(url)
+    innerTextSocket.onmessage = event => {
+        element.innerText = event.data
+    }
+})(document.body.appendChild(document.createElement('p')));
 
-const randomNumberElement = document.createElement('p')
-url.pathname = 'randomNumberInnerText'
-const randomNumberInnerTextSocket = new WebSocket(url)
-randomNumberInnerTextSocket.onmessage = event => {
-    randomNumberElement.innerText = event.data
-}
-document.body.appendChild(randomNumberElement)
+(element => {
+    url.pathname = 'randomNumberInnerText'
+    const innerTextSocket = new WebSocket(url)
+    innerTextSocket.onmessage = event => {
+        element.innerText = event.data
+    }
+})(document.body.appendChild(document.createElement('p')));
 
 const cursorElement = document.createElement('p')
 cursorElement.innerText = 'cursor: undefined'
-document.body.appendChild(cursorElement)
+document.body.appendChild(cursorElement);
 
-const histogramSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-histogramSVGElement.setAttribute('width', '400')
-histogramSVGElement.setAttribute('height', '300')
-histogramSVGElement.setAttribute('viewBox', '0 0 560 420')
-url.pathname = 'histogramSVGInnerHTML'
-const histogramSVGInnerHTMLSocket = new WebSocket(url)
-histogramSVGInnerHTMLSocket.onmessage = event => {
-    histogramSVGElement.innerHTML = event.data
-}
-histogramSVGElement.onmousemove = ev => {
-    const axes = histogramSVGElement.firstChild
-
-    const xInPixels = ev.offsetX * 560 / 400
-    const xInNormalized = (xInPixels - axes.dataset.xminInPixels)
-        / (axes.dataset.xmaxInPixels - axes.dataset.xminInPixels)
-    const xInData = Number(axes.dataset.xminInData)
-        + xInNormalized * (axes.dataset.xmaxInData - axes.dataset.xminInData)
-
-    const yInPixels = ev.offsetY * 420 / 300
-    const yInNormalized = (axes.dataset.yminInPixels - yInPixels)
-        / (axes.dataset.yminInPixels - axes.dataset.ymaxInPixels)
-    const yInData = Number(axes.dataset.yminInData)
-        + yInNormalized * (axes.dataset.ymaxInData - axes.dataset.yminInData)
-
-    if (xInNormalized < 0 || xInNormalized > 1) {
-        cursorElement.innerText = `cursor: undefined`
-        return
+(element => {
+    element.setAttribute('width', '400')
+    element.setAttribute('height', '300')
+    element.setAttribute('viewBox', '0 0 560 420')
+    url.pathname = 'histogramSVGInnerHTML'
+    const innerHTMLSocket = new WebSocket(url)
+    innerHTMLSocket.onmessage = event => {
+        element.innerHTML = event.data
     }
-    if (yInNormalized < 0 || yInNormalized > 1) {
-        cursorElement.innerText = `cursor: undefined`
-        return
+    element.onmousemove = ev => {
+        const axes = element.firstChild
+
+        const xInPixels = ev.offsetX * 560 / 400
+        const xInNormalized = (xInPixels - axes.dataset.xminInPixels)
+            / (axes.dataset.xmaxInPixels - axes.dataset.xminInPixels)
+        const xInData = Number(axes.dataset.xminInData)
+            + xInNormalized * (axes.dataset.xmaxInData - axes.dataset.xminInData)
+
+        const yInPixels = ev.offsetY * 420 / 300
+        const yInNormalized = (axes.dataset.yminInPixels - yInPixels)
+            / (axes.dataset.yminInPixels - axes.dataset.ymaxInPixels)
+        const yInData = Number(axes.dataset.yminInData)
+            + yInNormalized * (axes.dataset.ymaxInData - axes.dataset.yminInData)
+
+        if (xInNormalized < 0 || xInNormalized > 1) {
+            cursorElement.innerText = `cursor: undefined`
+            return
+        }
+        if (yInNormalized < 0 || yInNormalized > 1) {
+            cursorElement.innerText = `cursor: undefined`
+            return
+        }
+
+        cursorElement.innerText = `cursor: {x: ${xInData}, y: ${yInData}}`
     }
+})(document.body.appendChild(document.createElementNS('http://www.w3.org/2000/svg', 'svg')));
 
-    cursorElement.innerText = `cursor: {x: ${xInData}, y: ${yInData}}`
-}
-document.body.appendChild(histogramSVGElement)
-
-const timeSeriesSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-timeSeriesSVGElement.setAttribute('width', '400')
-timeSeriesSVGElement.setAttribute('height', '300')
-timeSeriesSVGElement.setAttribute('viewBox', '0 0 560 420')
-url.pathname = 'timeSeriesSVGInnerHTML'
-const timeSeriesSVGInnerHTMLSocket = new WebSocket(url)
-timeSeriesSVGInnerHTMLSocket.onmessage = event => {
-    timeSeriesSVGElement.innerHTML = event.data
-}
-document.body.appendChild(timeSeriesSVGElement)
 
