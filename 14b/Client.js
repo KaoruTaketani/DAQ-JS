3a02cd4ee2aee809fb5a840cd7292e3bf85eb294
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-09-02 12:30:21 +0900

    changed formats of Client.js in 14b

--- 14b/Client.js
+++ 14b/Client.js
@@ -6,107 +6,108 @@ socket.onclose = () => {
     document.body.innerHTML = "the connection was closed by the server."
 }
 
-const startButtonElement = document.createElement('input')
-startButtonElement.type = 'button'
-startButtonElement.value = 'start'
-startButtonElement.style.width = '130px'
-startButtonElement.onclick = () => {
-    socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: true }))
-}
-url.pathname = 'startButtonDisabled'
-const startButtonDisabledSocket = new WebSocket(url)
-startButtonDisabledSocket.onmessage = event => {
-    startButtonElement.disabled = event.data === 'true'
-}
-document.body.appendChild(startButtonElement)
+(element => {
+    element.type = 'button'
+    element.value = 'start'
+    element.style.width = '130px'
+    element.onclick = () => {
+        socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: true }))
+    }
+    url.pathname = 'startButtonDisabled'
+    const disabledSocket = new WebSocket(url)
+    disabledSocket.onmessage = event => {
+        element.disabled = event.data === 'true'
+    }
+})(document.body.appendChild(document.createElement('input')));
 
-const stopButtonElement = document.createElement('input')
-stopButtonElement.type = 'button'
-stopButtonElement.value = 'stop'
-stopButtonElement.style.width = '130px'
-stopButtonElement.onclick = () => {
-    socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: false }))
-}
-url.pathname = 'stopButtonDisabled'
-const stopButtonDisabledSocket = new WebSocket(url)
-stopButtonDisabledSocket.onmessage = event => {
-    stopButtonElement.disabled = event.data === 'true'
-}
-document.body.appendChild(stopButtonElement)
+(element => {
+    element.type = 'button'
+    element.value = 'stop'
+    element.style.width = '130px'
+    element.onclick = () => {
+        socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: false }))
+    }
+    url.pathname = 'stopButtonDisabled'
+    const disabledSocket = new WebSocket(url)
+    disabledSocket.onmessage = event => {
+        element.disabled = event.data === 'true'
+    }
+})(document.body.appendChild(document.createElement('input')));
 
-const startTimeElement = document.createElement('p')
-url.pathname = 'startTimeInnerText'
-const startTimeInnerTextSocket = new WebSocket(url)
-startTimeInnerTextSocket.onmessage = event => {
-    startTimeElement.innerText = event.data
-}
-document.body.appendChild(startTimeElement)
+(element => {
+    url.pathname = 'startTimeInnerText'
+    const innerTextSocket = new WebSocket(url)
+    innerTextSocket.onmessage = event => {
+        element.innerText = event.data
+    }
+})(document.body.appendChild(document.createElement('p')));
 
-const randomNumberElement = document.createElement('p')
-url.pathname = 'randomNumberInnerText'
-const randomNumberInnerTextSocket = new WebSocket(url)
-randomNumberInnerTextSocket.onmessage = event => {
-    randomNumberElement.innerText = event.data
-}
-document.body.appendChild(randomNumberElement)
+(element => {
+    url.pathname = 'randomNumberInnerText'
+    const innerTextSocket = new WebSocket(url)
+    innerTextSocket.onmessage = event => {
+        element.innerText = event.data
+    }
+})(document.body.appendChild(document.createElement('p')));
 
-const histogramSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-histogramSVGElement.setAttribute('width', '400')
-histogramSVGElement.setAttribute('height', '300')
-histogramSVGElement.setAttribute('viewBox', '0 0 560 420')
-url.pathname = 'histogramSVGInnerHTML'
-const histogramSVGInnerHTMLSocket = new WebSocket(url)
-histogramSVGInnerHTMLSocket.onmessage = event => {
-    histogramSVGElement.innerHTML = event.data
-}
-histogramSVGElement.ondblclick = () => {
-    dialogElement.showModal()
-}
-document.body.appendChild(histogramSVGElement)
+(element => {
+    element.setAttribute('width', '400')
+    element.setAttribute('height', '300')
+    element.setAttribute('viewBox', '0 0 560 420')
+    element.ondblclick = () => {
+        dialogElement.showModal()
+    }
+    url.pathname = 'histogramSVGInnerHTML'
+    const innerHTMLSocket = new WebSocket(url)
+    innerHTMLSocket.onmessage = event => {
+        element.innerHTML = event.data
+    }
+})(document.body.appendChild(document.createElementNS('http://www.w3.org/2000/svg', 'svg')));
 
-
-const timeSeriesSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-timeSeriesSVGElement.setAttribute('width', '400')
-timeSeriesSVGElement.setAttribute('height', '300')
-timeSeriesSVGElement.setAttribute('viewBox', '0 0 560 420')
-url.pathname = 'timeSeriesSVGInnerHTML'
-const timeSeriesSVGInnerHTMLSocket = new WebSocket(url)
-timeSeriesSVGInnerHTMLSocket.onmessage = event => {
-    timeSeriesSVGElement.innerHTML = event.data
-}
-document.body.appendChild(timeSeriesSVGElement)
+(element => {
+    element.setAttribute('width', '400')
+    element.setAttribute('height', '300')
+    element.setAttribute('viewBox', '0 0 560 420')
+    url.pathname = 'timeSeriesSVGInnerHTML'
+    const innerHTMLSocket = new WebSocket(url)
+    innerHTMLSocket.onmessage = event => {
+        element.innerHTML = event.data
+    }
+})(document.body.appendChild(document.createElementNS('http://www.w3.org/2000/svg', 'svg')));
 
 const dialogElement = document.createElement('dialog')
-document.body.appendChild(dialogElement)
+document.body.appendChild(dialogElement);
 
-const downloadHDF5ButtonElement = document.createElement('input')
-downloadHDF5ButtonElement.type = 'button'
-downloadHDF5ButtonElement.value = 'download hdf5'
-downloadHDF5ButtonElement.style.width = '130px'
-downloadHDF5ButtonElement.style.display = 'block'
-dialogElement.appendChild(downloadHDF5ButtonElement)
+(element => {
+    element.type = 'button'
+    element.value = 'download hdf5'
+    element.style.width = '130px'
+    element.style.display = 'block'
+    const linkElement = document.createElement('a')
+    url.pathname = 'hdf5LinkHref'
+    const hrefSocket = new WebSocket(url)
+    hrefSocket.onmessage = event => {
+        linkElement.href = event.data
+    }
+    element.onclick = () => {
+        linkElement.setAttribute('download', 'histogram.h5')
+        linkElement.click()
+    }
+})(dialogElement.appendChild(document.createElement('input')));
 
-const hdf5LinkElement = document.createElement('a')
-url.pathname = 'hdf5LinkHref'
-const hdf5LinkHrefSocket = new WebSocket(url)
-hdf5LinkHrefSocket.onmessage = event => {
-    hdf5LinkElement.href = event.data
-}
-downloadHDF5ButtonElement.onclick = () => {
-    hdf5LinkElement.setAttribute('download', 'histogram.h5')
-    hdf5LinkElement.click()
-}
+(element => {
+    element.type = 'button'
+    element.value = 'close'
+    element.style.width = '130px'
+    element.style.display = 'block'
+    element.onclick = () => {
+        dialogElement.close()
+    }
+})(dialogElement.appendChild(document.createElement('input')));
 
 
-const closeButtonElement = document.createElement('input')
-closeButtonElement.type = 'button'
-closeButtonElement.value = 'close'
-closeButtonElement.style.width = '130px'
-closeButtonElement.style.display = 'block'
-closeButtonElement.onclick = () => {
-    dialogElement.close()
-}
-dialogElement.appendChild(closeButtonElement)
+
+
 
 
 
