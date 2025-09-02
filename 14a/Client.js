0c88f8382b3742f8570d9d69c9b21480ec618705
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-09-02 12:26:47 +0900

    changed formats of Client.js in 14a

--- 14a/Client.js
+++ 14a/Client.js
@@ -6,103 +6,107 @@ socket.onclose = () => {
     document.body.innerHTML = "the connection was closed by the server."
 }
 
-const startButtonElement = document.createElement('input')
-startButtonElement.type = 'button'
-startButtonElement.value = 'start'
-startButtonElement.style.width = '130px'
-startButtonElement.onclick = () => {
-    socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: true }))
-}
-url.pathname = 'startButtonDisabled'
-const startButtonDisabledSocket = new WebSocket(url)
-startButtonDisabledSocket.onmessage = event => {
-    startButtonElement.disabled = event.data === 'true'
-}
-document.body.appendChild(startButtonElement)
+(element => {
+    element.type = 'button'
+    element.value = 'start'
+    element.style.width = '130px'
+    element.onclick = () => {
+        socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: true }))
+    }
+    url.pathname = 'startButtonDisabled'
+    const disabledSocket = new WebSocket(url)
+    disabledSocket.onmessage = event => {
+        element.disabled = event.data === 'true'
+    }
+})(document.body.appendChild(document.createElement('input')));
 
-const stopButtonElement = document.createElement('input')
-stopButtonElement.type = 'button'
-stopButtonElement.value = 'stop'
-stopButtonElement.style.width = '130px'
-stopButtonElement.onclick = () => {
-    socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: false }))
-}
-url.pathname = 'stopButtonDisabled'
-const stopButtonDisabledSocket = new WebSocket(url)
-stopButtonDisabledSocket.onmessage = event => {
-    stopButtonElement.disabled = event.data === 'true'
-}
-document.body.appendChild(stopButtonElement)
+(element => {
+    element.type = 'button'
+    element.value = 'stop'
+    element.style.width = '130px'
+    element.onclick = () => {
+        socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: false }))
+    }
+    url.pathname = 'stopButtonDisabled'
+    const disabledSocket = new WebSocket(url)
+    disabledSocket.onmessage = event => {
+        element.disabled = event.data === 'true'
+    }
+})(document.body.appendChild(document.createElement('input')));
 
-const startTimeElement = document.createElement('p')
-url.pathname = 'startTimeInnerText'
-const startTimeInnerTextSocket = new WebSocket(url)
-startTimeInnerTextSocket.onmessage = event => {
-    startTimeElement.innerText = event.data
-}
-document.body.appendChild(startTimeElement)
+(element => {
+    url.pathname = 'startTimeInnerText'
+    const innerTextSocket = new WebSocket(url)
+    innerTextSocket.onmessage = event => {
+        element.innerText = event.data
+    }
+})(document.body.appendChild(document.createElement('p')));
 
-const randomNumberElement = document.createElement('p')
-url.pathname = 'randomNumberInnerText'
-const randomNumberInnerTextSocket = new WebSocket(url)
-randomNumberInnerTextSocket.onmessage = event => {
-    randomNumberElement.innerText = event.data
-}
-document.body.appendChild(randomNumberElement)
+(element => {
+    url.pathname = 'randomNumberInnerText'
+    const innerTextSocket = new WebSocket(url)
+    innerTextSocket.onmessage = event => {
+        element.innerText = event.data
+    }
+})(document.body.appendChild(document.createElement('p')));
 
-const histogramSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-histogramSVGElement.setAttribute('width', '400')
-histogramSVGElement.setAttribute('height', '300')
-histogramSVGElement.setAttribute('viewBox', '0 0 560 420')
-histogramSVGElement.ondblclick = () => {
-    dialogElement.showModal()
-}
-url.pathname = 'histogramSVGInnerHTML'
-const histogramSVGInnerHTMLSocket = new WebSocket(url)
-histogramSVGInnerHTMLSocket.onmessage = event => {
-    histogramSVGElement.innerHTML = event.data
-}
-document.body.appendChild(histogramSVGElement)
+(element => {
+    element.setAttribute('width', '400')
+    element.setAttribute('height', '300')
+    element.setAttribute('viewBox', '0 0 560 420')
+    element.ondblclick = () => {
+        dialogElement.showModal()
+    }
+    url.pathname = 'histogramSVGInnerHTML'
+    const innerHTMLSocket = new WebSocket(url)
+    innerHTMLSocket.onmessage = event => {
+        element.innerHTML = event.data
+    }
+})(document.body.appendChild(document.createElementNS('http://www.w3.org/2000/svg', 'svg')));
 
-const timeSeriesSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-timeSeriesSVGElement.setAttribute('width', '400')
-timeSeriesSVGElement.setAttribute('height', '300')
-timeSeriesSVGElement.setAttribute('viewBox', '0 0 560 420')
-url.pathname = 'timeSeriesSVGInnerHTML'
-const timeSeriesSVGInnerHTMLSocket = new WebSocket(url)
-timeSeriesSVGInnerHTMLSocket.onmessage = event => {
-    timeSeriesSVGElement.innerHTML = event.data
-}
-document.body.appendChild(timeSeriesSVGElement)
+(element => {
+    element.setAttribute('width', '400')
+    element.setAttribute('height', '300')
+    element.setAttribute('viewBox', '0 0 560 420')
+    url.pathname = 'timeSeriesSVGInnerHTML'
+    const innerHTMLSocket = new WebSocket(url)
+    innerHTMLSocket.onmessage = event => {
+        element.innerHTML = event.data
+    }
+})(document.body.appendChild(document.createElementNS('http://www.w3.org/2000/svg', 'svg')));
+
+
 
 
 
 const dialogElement = document.createElement('dialog')
-document.body.appendChild(dialogElement)
+document.body.appendChild(dialogElement);
 
-const downloadSVGButtonElement = document.createElement('input')
-downloadSVGButtonElement.type = 'button'
-downloadSVGButtonElement.value = 'download svg'
-downloadSVGButtonElement.style.width = '130px'
-downloadSVGButtonElement.style.display = 'block'
-const svgLinkElement = document.createElement('a')
-downloadSVGButtonElement.onclick = () => {
-    svgLinkElement.setAttribute('href', 'data:image/svg+xml;base64,' + window.btoa(
-        `<svg xmlns="http://www.w3.org/2000/svg" >${histogramSVGElement.innerHTML}</svg>`
-    ))
-    svgLinkElement.setAttribute('download', 'histogram.svg')
-    svgLinkElement.click()
-}
-dialogElement.appendChild(downloadSVGButtonElement)
+(element => {
+    element.type = 'button'
+    element.value = 'download svg'
+    element.style.width = '130px'
+    element.style.display = 'block'
+    const linkElement = document.createElement('a')
+    element.onclick = () => {
+        linkElement.setAttribute('href', 'data:image/svg+xml;base64,' + window.btoa(
+            `<svg xmlns="http://www.w3.org/2000/svg" >${element.innerHTML}</svg>`
+        ))
+        linkElement.setAttribute('download', 'histogram.svg')
+        linkElement.click()
+    }
+})(dialogElement.appendChild(document.createElement('input')));
 
-const closeButtonElement = document.createElement('input')
-closeButtonElement.type = 'button'
-closeButtonElement.value = 'close'
-closeButtonElement.style.width = '130px'
-closeButtonElement.style.display = 'block'
-closeButtonElement.onclick = () => {
-    dialogElement.close()
-}
-dialogElement.appendChild(closeButtonElement)
+(element => {
+    element.type = 'button'
+    element.value = 'close'
+    element.style.width = '130px'
+    element.style.display = 'block'
+    element.onclick = () => {
+        dialogElement.close()
+    }
+})(dialogElement.appendChild(document.createElement('input')));
+
+
 
 
