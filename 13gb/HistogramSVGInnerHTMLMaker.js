2a5d2c1c281c756774c5fa46b3aa559423efc146
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-09-17 10:52:32 +0900

    cleanup histogram svg inner html maker from 13d

--- 13gb/HistogramSVGInnerHTMLMaker.js
+++ 13gb/HistogramSVGInnerHTMLMaker.js
@@ -18,15 +18,18 @@ export default class extends Operator {
             this._operation()
         })
         this._operation = () => {
-            const ax = {
-                xLim: this._histogram.binLimits,
-                yLim: [0.1, max(this._histogram.binCounts)],
-                xTick: linspace(this._histogram.binLimits[0], this._histogram.binLimits[1], this._histogram.binCounts.length + 1),
-                yTick: [0.1, max(this._histogram.binCounts)],
-                xTickLabel: linspace(this._histogram.binLimits[0], this._histogram.binLimits[1], this._histogram.binCounts.length + 1).map(x => x.toFixed(1)),
-                yTickLabel: ['0.1', `${max(this._histogram.binCounts)}`],
-                yScale: 'log'
-            }
+            const yMax = max(this._histogram.binCounts) === 0
+                ? 1 : max(this._histogram.binCounts),
+                xTick =linspace(this._histogram.binLimits[0], this._histogram.binLimits[1], this._histogram.binCounts.length + 1),
+                ax = {
+                    xLim: this._histogram.binLimits,
+                    yLim: [0.1, yMax],
+                    xTick: xTick,
+                    yTick: [0.1, yMax],
+                    xTickLabel: xTick.map(x => x.toFixed(1)),
+                    yTickLabel: ['0.1', `${yMax}`],
+                    yScale: 'log'
+                }
             variables.histogramSVGInnerHTML.assign([
                 axes(ax),
                 xlabel(ax, 'random number'),
