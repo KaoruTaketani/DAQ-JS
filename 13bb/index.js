649faf5c41d7c0244b1ffe4ce7bf38d047cf852c
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-10-11 14:39:46 +0900

    modified 13bb to use http put handler

--- 13bb/index.js
+++ 13bb/index.js
@@ -1,17 +1,18 @@
 import { Server } from 'http'
+import RandomNumberGenerator from '../13//RandomNumberGenerator.js'
 import HistogramInitializer from '../13/HistogramInitializer.js'
 import HistogramMaker from '../13/HistogramMaker.js'
 import HistogramSVGInnerHTMLMaker from '../13/HistogramSVGInnerHTMLMaker.js'
 import HTTPGetHandler from '../13/HTTPGetHandler.js'
+import HTTPPutHandler from '../13/HTTPPutHandler.js'
 import HTTPServerSetupper from '../13/HTTPServerSetupper.js'
 import HTTPUpgradeHandler from '../13/HTTPUpgradeHandler.js'
-import RandomNumberGenerator from '../13//RandomNumberGenerator.js'
 import RandomNumberInnerTextChanger from '../13/RandomNumberInnerTextChanger.js'
 import StartButtonDisabledChanger from '../13/StartButtonDisabledChanger.js'
 import StopButtonDisabledChanger from '../13/StopButtonDisabledChanger.js'
-import StartTimeInnerTextChanger from '../13ba/StartTimeInnerTextChanger.js'
-import StartTimeChanger from '../13ba/StartTimeChanger.js'
 import Variables from '../13/Variables.js'
+import StartTimeChanger from '../13ba/StartTimeChanger.js'
+import StartTimeInnerTextChanger from '../13ba/StartTimeInnerTextChanger.js'
 
 const variables = new Variables()
 
@@ -19,6 +20,7 @@ new HistogramInitializer(variables)
 new HistogramMaker(variables)
 new HistogramSVGInnerHTMLMaker(variables)
 new HTTPGetHandler(variables)
+new HTTPPutHandler(variables)
 new HTTPServerSetupper(variables)
 new HTTPUpgradeHandler(variables)
 new RandomNumberGenerator(variables)
