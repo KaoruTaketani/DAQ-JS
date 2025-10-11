93f23a238fb21a5d3a85cdcef186e8aef4cc0fce
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-10-11 14:58:05 +0900

    modified 13cc to use http put handler

--- 13cc/RandomNumberGeneratorClient.js
+++ 13cc/RandomNumberGeneratorClient.js
@@ -11,7 +11,9 @@ socket.onclose = () => {
     element.value = 'start'
     element.style.width = '130px'
     element.onclick = () => {
-        socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: true }))
+        const xhr = new XMLHttpRequest()
+        xhr.open('PUT','/?randomNumberGeneratorIsBusy=true')
+        xhr.send()
     }
     url.pathname = 'startButtonDisabled'
     const disabledSocket = new WebSocket(url)
@@ -26,7 +28,9 @@ socket.onclose = () => {
     element.style.width = '130px'
     element.disabled = true
     element.onclick = () => {
-        socket.send(JSON.stringify({ randomNumberGeneratorIsBusy: false }))
+        const xhr = new XMLHttpRequest()
+        xhr.open('PUT','/?randomNumberGeneratorIsBusy=false')
+        xhr.send()
     }
     url.pathname = 'stopButtonDisabled'
     const disabledSocket = new WebSocket(url)
