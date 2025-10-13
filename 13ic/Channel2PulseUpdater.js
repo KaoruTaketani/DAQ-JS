c9a197f86704b272a1691a13d0085f1d6e9208d0
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-10-13 14:43:40 +0900

    refactored channel 2 pulse updator

--- 13ic/Channel2PulseUpdater.js
+++ 13ic/Channel2PulseUpdater.js
@@ -16,35 +16,22 @@ export default class extends Operator {
             this._operation()
         })
         this._operation = () => {
-            // console.log(`pulse: ${this._channel2Pulse}, dest: ${this._channel2Destination}`)
-            if (Number.isNaN(this._channel2Destination)) {
-                if (!Number.isNaN(this._channel2Pulse)) return
+            if (Number.isNaN(this._channel2Destination)
+                && !Number.isNaN(this._channel2Pulse)) return
 
-                this._socketQueue.push((socket, done) => {
-                    socket.once('data', data => {
-                        console.log(`2nd data: ${data}`)
-                        const pulse = parseInt(data.split(' ')[1])
-                        variables.channel2Pulse.assign(pulse)
-                        // socket.end()
-                        done()
-                    }).write('pulse?:2')
-                })
-            } else {
-                // console.log('repeat?')
-                this._socketQueue.push((socket, done) => {
-                    socket.once('data', data => {
-                        console.log(`2nd data: ${data}`)
-                        const pulse = parseInt(data.split(' ')[1]),
-                            isBusy = data.split(' ')[2] == '1'
-                        variables.channel2Pulse.assign(pulse)
-                        if(!isBusy){
-                            variables.channel2Destination.assign(Number.NaN)
-                        }
-                        // socket.end()
-                        done()
-                    }).write('pulse?:2')
-                })
-            }
+            this._socketQueue.push((socket, done) => {
+                socket.once('data', data => {
+                    console.log(`2nd data: ${data}`)
+                    const pulse = parseInt(data.split(' ')[1]),
+                        isBusy = data.split(' ')[2] == '1'
+                    variables.channel2Pulse.assign(pulse)
+                    if (!isBusy) {
+                        variables.channel2Destination.assign(Number.NaN)
+                    }
+                    // socket.end()
+                    done()
+                }).write('pulse?:2')
+            })
         }
     }
 }
