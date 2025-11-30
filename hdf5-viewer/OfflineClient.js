1942e4d7f11aba8ff4db611bd4ae313702c62c99
Kaoru Taketani <kaoru.taketani@gmail.com>
2025-12-01 08:18:14 +0900

    added im2src

--- hdf5-viewer/OfflineClient.js
+++ hdf5-viewer/OfflineClient.js
@@ -1,6 +1,7 @@
 import h5wasm from "./node_modules/h5wasm/dist/esm/hdf5_hl.js";
 // import h5wasm from "https://cdn.jsdelivr.net/npm/h5wasm/dist/esm/hdf5_hl.js";
 import imagesc from "../lib/imagesc.js";
+import im2src from "../lib/im2src.js";
 const { FS } = await h5wasm.ready;
 
 let response = await fetch("./sans59510.nxs.ngv.h5");
@@ -50,28 +51,9 @@ console.log(f.attrs);
                 if (!ctx) return
                 // imagedata(h)
                 const im = imagesc(h)
-                const imagedata = new ImageData(dataset.shape[0], dataset.shape[1]);
-
-                for (var y = 0; y < im.height; y++) {
-                    for (var x = 0; x < im.width; x++) {
-                        const rgb = im.data[y * (im.width + 1) + x + 1] // x=0 corresponds null data for png
-                        imagedata.data[(y * im.width + x) * 4 + 0] = rgb;  // R
-                        imagedata.data[(y * im.width + x) * 4 + 1] = rgb;  // G
-                        imagedata.data[(y * im.width + x) * 4 + 2] = rgb;  // B
-                        imagedata.data[(y * im.width + x) * 4 + 3] = 255;  // Alpha
-                    }
-                }
-
-                // var tmp = document.createElement('canvas');
-                var ctxInvisible = invisibleCanvasElement.getContext('2d');
-                if (!ctxInvisible) return
-                // console.log('new')
-                invisibleCanvasElement.width = imagedata.width;
-                invisibleCanvasElement.height = imagedata.height;
-                ctxInvisible.putImageData(imagedata, 0, 0);
 
                 var image = new Image();
-                image.src = invisibleCanvasElement.toDataURL();
+                image.src = im2src(im,invisibleCanvasElement);
                 ctx.imageSmoothingEnabled = false
                 ctx.drawImage(image, 0, 0, canvasElement.width, canvasElement.height)
                 // f.close()
