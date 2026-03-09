d2bbb9603a0bfae61822c707c7534636a5381ace
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-03-09 11:23:34 +0900

    set top/left of draw image manualy

--- hdf5-viewer/FilteredImageClient.js
+++ hdf5-viewer/FilteredImageClient.js
@@ -62,7 +62,9 @@ const canvasElement = document.body.appendChild(document.createElement('canvas')
 
         ctx.imageSmoothingEnabled = false
         // ctx.drawImage(element, 0, 0, canvasElement.width, canvasElement.height)
-        ctx.drawImage(element, 0, 0)
+        // ctx.drawImage(element, 0, 0)
+        // x:[73, 503], y: [374,32]
+        ctx.drawImage(element, 73 * 400/560, 32 * 300/420)
     });
 })(document.createElement('img'));
 
