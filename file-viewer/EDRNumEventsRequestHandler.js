b9388523d096d3372ed240a22385545181f0e439
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-03-13 10:17:52 +0900

    added edr channel request handler

--- file-viewer/EDRNumEventsRequestHandler.js
+++ file-viewer/EDRNumEventsRequestHandler.js
@@ -1,4 +1,4 @@
-import { statSync } from 'fs'
+import { openSync, closeSync, readSync } from 'fs'
 import { join } from 'path'
 
 export default class {
@@ -20,10 +20,121 @@ export default class {
         })
         this._operation = () => {
             if (!this._url.pathname.endsWith('.edr')) return
-            if (this._url.searchParams.get('type') !== 'numEvents') return
+            if (this._url.searchParams.get('type') !== 'channel') return
 
-            const stat = statSync(join(this._edrPath, this._url.pathname))
-            this._response.end(`${(stat.size / 8).toLocaleString()} events`)
+            const offsetValue = this._url.searchParams.get('offset')
+            if (!offsetValue) return
+            const offset = parseInt(offsetValue)
+
+            const fd = openSync(join(this._edrPath, this._url.pathname), 'r')
+            const chunk = new Uint8Array(8 * 25)
+            readSync(fd, chunk, 0, 8 * 25, 8 * offset)
+            closeSync(fd)
+
+            const channelEvents = new Array(25)
+            for (let i = 0; i < chunk.length / 8; ++i) {
+                if (chunk[8 * i] === 0x5a) {
+                    const
+                        byte1 = chunk[8 * i + 1],
+                        byte2 = chunk[8 * i + 2],
+                        byte3 = chunk[8 * i + 3],
+                        byte4 = chunk[8 * i + 4],
+                        byte5 = chunk[8 * i + 5],
+                        byte6 = chunk[8 * i + 6],
+                        byte7 = chunk[8 * i + 7],
+                        tof = ((byte1 << 16) + (byte2 << 8) + byte3) * 25, /** time bin is 25 nsec */
+                        channel = byte4 & 0b111,
+                        module = byte4 >> 3,
+                        left = (byte5 << 4) + (byte6 >> 4),
+                        right = ((byte6 & 0b1111) << 8) + byte7
+
+                    channelEvents[i] = {
+                        header: `0x5a`,
+                        channel: channel,
+                        module: module,
+                        tofInNanoseconds: tof,
+                        left: left,
+                        right: right
+                    }
+                } else if (chunk[8 * i] === 0x5b) {
+                    channelEvents[i] = {
+                        header: `0x5b`,
+                        channel: Number.NaN,
+                        module: Number.NaN,
+                        tofInNanoseconds: Number.NaN,
+                        left: Number.NaN,
+                        right: Number.NaN
+                    }
+                } else if (chunk[8 * i] === 0x5c) {
+                    channelEvents[i] = {
+                        header: `0x5c`,
+                        channel: Number.NaN,
+                        module: Number.NaN,
+                        tofInNanoseconds: Number.NaN,
+                        left: Number.NaN,
+                        right: Number.NaN
+                    }
+                } else {
+                    // unexpected
+                }
+            }
+            // necessary to make paired/neutron events from channel events
+            // this.variables.neutronPositionBitLength.assign(10)
+            // this.variables.tofMaxInMilliseconds.assign(80)
+            // this.variables.tofDifferenceMaxInNanoseconds.assign(250)
+            // this.variables.tofDifferenceMinInNanoseconds.assign(-250)
+            //
+            // pulseHeight: left + right,
+            // coordinateInPixels: ((left << 8) / (left + right)) >> 0
+            //
+            // if (this._channelEvent.channel === 0) this._channel0Event = this._channelEvent
+            // if (this._channelEvent.channel === 1) this._channel1Event = this._channelEvent
+
+            // if (!this._channel0Event) return
+            // if (!this._channel1Event) return
+
+            // variables.pairedEvent.assign({
+            //     xCoordinateInPixels: this._channel0Event.coordinateInPixels,
+            //     xPulseHeight: this._channel0Event.pulseHeight,
+            //     xTOFInNanoseconds: this._channel0Event.tofInNanoseconds,
+            //     yCoordinateInPixels: this._channel1Event.coordinateInPixels,
+            //     yPulseHeight: this._channel1Event.pulseHeight,
+            //     yTOFInNanoseconds: this._channel1Event.tofInNanoseconds
+            // })
+
+            // const dt = this._pairedEvent.xTOFInNanoseconds - this._pairedEvent.yTOFInNanoseconds
+            // if (dt > this._tofDifferenceMaxInNanoseconds) return
+            // if (dt < this._tofDifferenceMinInNanoseconds) return
+
+            // const tof = (this._pairedEvent.xTOFInNanoseconds + this._pairedEvent.yTOFInNanoseconds) >> 1
+            // if (tof > this._tofMaxInMilliseconds * 1_000_000) return
+
+            // variables.neutronEvent.assign({
+            //     tofInNanoseconds: tof,
+            //     xCoordinateInPixels: this._pairedEvent.xCoordinateInPixels,
+            //     yCoordinateInPixels: this._pairedEvent.yCoordinateInPixels,
+            //     // channelEvent.pulse is 12bit, pairedEvent.pulse is sum of them.
+            //     // to match the pulseHieghtHistogram's 10bit bints, divide here 2^4=16
+            //     pulseheight: (this._pairedEvent.xPulseHeight + this._pairedEvent.yPulseHeight) / 16
+            // })
+
+            this._response.end([
+                '<thead>',
+                '<tr>',
+                Object.keys(channelEvents[0]).map(key => `<th>${key}</th>`).join(''),
+                '</tr>',
+                '</thead>',
+                '<tbody align="right">',
+                channelEvents.map(obj => ['<tr>',
+                    Object.keys(obj).map(key => {
+                        /** @type {any} */
+                        const tmp = obj
+                        return `<td>${tmp[key].toLocaleString()}</td>`
+                    }).join(''),
+                    '</tr>'].join('')
+                ).join(''),
+                '</tbody>'
+            ].join(''))
         }
     }
 }
--- file-viewer/index.js
+++ file-viewer/index.js
@@ -1,13 +1,15 @@
-import { closeSync, openSync, readdir, readFile, readSync, statSync } from 'fs'
+import { readFile } from 'fs'
 import { Server } from 'http'
-import { basename, join } from 'path'
-import Variables from './Variables.js'
-import FilesRequestHandler from './FilesRequestHandler.js'
+import { basename } from 'path'
+import EDRChannelRequestHandler from './EDRChannelRequestHandler.js'
 import EDRNumEventsRequestHandler from './EDRNumEventsRequestHandler.js'
+import FilesRequestHandler from './FilesRequestHandler.js'
+import Variables from './Variables.js'
 
 const variables = new Variables()
 new FilesRequestHandler(variables)
 new EDRNumEventsRequestHandler(variables)
+new EDRChannelRequestHandler(variables)
 
 const httpServer = new Server()
 const edrPath = '../../edr'
@@ -17,133 +19,8 @@ httpServer.on('request', (request, response) => {
     console.log(`GET url: ${request.url}`)
     const url = new URL(`http://localhost${request.url}`)
     variables.response.assign(response)
-    variables.url.assign(new URL(`http://localhost${request.url}`))
-
-    if (url.pathname.endsWith('.edr')) {
-        console.log(`pathname: ${url.pathname}, type: ${url.searchParams.get('type')}, offset: ${url.searchParams.get('offset')}`)
-        if (url.searchParams.get('type') === 'channel') {
-            const offsetValue = url.searchParams.get('offset')
-            if (!offsetValue) return
-            const offset = parseInt(offsetValue)
-
-            const fd = openSync(join(edrPath, url.pathname), 'r')
-            const chunk = new Uint8Array(8 * 25)
-            readSync(fd, chunk, 0, 8 * 25, 8 * offset)
-            closeSync(fd)
-
-            // createReadStream(join(edrPath, url.pathname), { start: 8 * offset, end: 8 * offset + 25 * 8, highWaterMark: 25 * 8 })
-            //     .on('data', chunk => {
-            const channelEvents = new Array(25)
-            for (let i = 0; i < chunk.length / 8; ++i) {
-                if (chunk[8 * i] === 0x5a) {
-                    const
-                        byte1 = chunk[8 * i + 1],
-                        byte2 = chunk[8 * i + 2],
-                        byte3 = chunk[8 * i + 3],
-                        byte4 = chunk[8 * i + 4],
-                        byte5 = chunk[8 * i + 5],
-                        byte6 = chunk[8 * i + 6],
-                        byte7 = chunk[8 * i + 7],
-                        tof = ((byte1 << 16) + (byte2 << 8) + byte3) * 25, /** time bin is 25 nsec */
-                        channel = byte4 & 0b111,
-                        module = byte4 >> 3,
-                        left = (byte5 << 4) + (byte6 >> 4),
-                        right = ((byte6 & 0b1111) << 8) + byte7
-
-                    channelEvents[i] = {
-                        header: `0x5a`,
-                        channel: channel,
-                        module: module,
-                        tofInNanoseconds: tof,
-                        left: left,
-                        right: right
-                    }
-                } else if (chunk[8 * i] === 0x5b) {
-                    channelEvents[i] = {
-                        header: `0x5b`,
-                        channel: Number.NaN,
-                        module: Number.NaN,
-                        tofInNanoseconds: Number.NaN,
-                        left: Number.NaN,
-                        right: Number.NaN
-                    }
-                } else if (chunk[8 * i] === 0x5c) {
-                    channelEvents[i] = {
-                        header: `0x5c`,
-                        channel: Number.NaN,
-                        module: Number.NaN,
-                        tofInNanoseconds: Number.NaN,
-                        left: Number.NaN,
-                        right: Number.NaN
-                    }
-                } else {
-                    // unexpected
-                }
-            }
-            // necessary to make paired/neutron events from channel events
-            // this.variables.neutronPositionBitLength.assign(10)
-            // this.variables.tofMaxInMilliseconds.assign(80)
-            // this.variables.tofDifferenceMaxInNanoseconds.assign(250)
-            // this.variables.tofDifferenceMinInNanoseconds.assign(-250)
-            //
-            // pulseHeight: left + right,
-            // coordinateInPixels: ((left << 8) / (left + right)) >> 0
-            //
-            // if (this._channelEvent.channel === 0) this._channel0Event = this._channelEvent
-            // if (this._channelEvent.channel === 1) this._channel1Event = this._channelEvent
-
-            // if (!this._channel0Event) return
-            // if (!this._channel1Event) return
-
-            // variables.pairedEvent.assign({
-            //     xCoordinateInPixels: this._channel0Event.coordinateInPixels,
-            //     xPulseHeight: this._channel0Event.pulseHeight,
-            //     xTOFInNanoseconds: this._channel0Event.tofInNanoseconds,
-            //     yCoordinateInPixels: this._channel1Event.coordinateInPixels,
-            //     yPulseHeight: this._channel1Event.pulseHeight,
-            //     yTOFInNanoseconds: this._channel1Event.tofInNanoseconds
-            // })
-
-            // const dt = this._pairedEvent.xTOFInNanoseconds - this._pairedEvent.yTOFInNanoseconds
-            // if (dt > this._tofDifferenceMaxInNanoseconds) return
-            // if (dt < this._tofDifferenceMinInNanoseconds) return
-
-            // const tof = (this._pairedEvent.xTOFInNanoseconds + this._pairedEvent.yTOFInNanoseconds) >> 1
-            // if (tof > this._tofMaxInMilliseconds * 1_000_000) return
-
-            // variables.neutronEvent.assign({
-            //     tofInNanoseconds: tof,
-            //     xCoordinateInPixels: this._pairedEvent.xCoordinateInPixels,
-            //     yCoordinateInPixels: this._pairedEvent.yCoordinateInPixels,
-            //     // channelEvent.pulse is 12bit, pairedEvent.pulse is sum of them.
-            //     // to match the pulseHieghtHistogram's 10bit bints, divide here 2^4=16
-            //     pulseheight: (this._pairedEvent.xPulseHeight + this._pairedEvent.yPulseHeight) / 16
-            // })
-
-            response.end([
-                '<thead>',
-                '<tr>',
-                Object.keys(channelEvents[0]).map(key => `<th>${key}</th>`).join(''),
-                '</tr>',
-                '</thead>',
-                '<tbody align="right">',
-                channelEvents.map(obj => ['<tr>',
-                    Object.keys(obj).map(key => {
-                        /** @type {any} */
-                        const tmp = obj
-                        return `<td>${tmp[key].toLocaleString()}</td>`
-                    }).join(''),
-                    '</tr>'].join('')
-                ).join(''),
-                '</tbody>'
-            ].join(''))
-
-            // }).on('end', () => {
-            //     console.log('end')
-            // })
-            return
-        }
-    }
+    variables.url.assign(url)
+    
     if (url.pathname.endsWith('.js')) {
         readFile(`.${request.url}`, 'utf8', (err, data) => {
             if (err) throw err
@@ -161,7 +38,7 @@ httpServer.on('request', (request, response) => {
             '    <meta charset="utf-8">',
             '</head>',
             '<body>',
-            `    <script type="module" src="./${basename(request.url, '.html')}.js">`,
+            `    <script type="module" src="./${basename(url.pathname, '.html')}.js">`,
             `    </script>`,
             '</body>',
             '</html>'
@@ -176,6 +53,7 @@ httpServer.on('request', (request, response) => {
             '    <meta charset="utf-8">',
             '</head>',
             '<body>',
+            `    <h2>EDR</h2>`,
             `    <p><a href="./ChannelEventTableClient.html">Channel</a></p>`,
             `    <p><a href="./PairedEventTableClient.html">Paired</a></p>`,
             `    <p><a href="./NeutronEventTableClient.html">Neutron</a></p>`,
