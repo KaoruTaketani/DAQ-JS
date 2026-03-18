6a45d7078c8bcd58269427bce2c91ff808663665
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-03-18 11:15:06 +0900

    changed timer get url

--- file-viewer/ChannelGetter.js
+++ file-viewer/ChannelGetter.js
@@ -26,6 +26,9 @@ export default class {
                 response.text().then(text => {
                     variables.tableInnerHTML.assign(text)
                 })
+            }).catch(() => {
+                variables.divInnerText.assign('failed to get')
+                variables.tableInnerHTML.assign('')
             })
         }
     }
