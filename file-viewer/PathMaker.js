53fa79638d49bf791139ac59328583feda3f0a1d
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-03-23 15:51:52 +0900

    modified path maker's logic

--- file-viewer/PathMaker.js
+++ file-viewer/PathMaker.js
@@ -16,11 +16,15 @@ export default class {
             if (!this._directoryName.endsWith('/')) return
 
             if (this._directoryName === '../') {
-                const newPath = this._path.substring(0, this._path.lastIndexOf('/'))
-                variables.path.assign(newPath === '' ? '/' : newPath)
+                // const newPath = this._path.substring(0, this._path.lastIndexOf('/'))
+                // variables.path.assign(newPath === '' ? '/' : newPath)
+                const tmp = this._path.slice(0, -1)
+                // console.log(tmp) // tmp is /20250424 if path is /20250424/
+                variables.path.assign(tmp.substring(0, tmp.lastIndexOf('/') + 1))
             } else {
-                const newPath = (this._path === '/' ? '' : this._path) + '/' + this._directoryName.substring(0, this._directoryName.length - 1)
-                variables.path.assign(newPath)
+                // const newPath = (this._path === '/' ? '' : this._path) + '/' + this._directoryName.substring(0, this._directoryName.length - 1)
+                // variables.path.assign(newPath)
+                variables.path.assign(this._path + this._directoryName)
             }
         }
     }
