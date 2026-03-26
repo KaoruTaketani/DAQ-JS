cbe03bd6809d5840739329d5e1e746ea83efe653
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-03-26 13:20:43 +0900

    used extended ../13/variables.js in 13fa

--- 13fa/Variables.js
+++ 13fa/Variables.js
@@ -3,27 +3,13 @@ import ElementBoolean from '../13/ElementBoolean.js'
 import ElementString from '../13/ElementString.js'
 import ListenableNumber from '../13/ListenableNumber.js'
 import ListenableObject from '../13/ListenableObject.js'
+import Variables from '../13/Variables.js'
 
-export default class {
+export default class extends Variables {
     constructor() {
-        this.httpServer = new ListenableObject()
-        this.requestParams = new ListenableObject()
-        this.webSocketPathnames = new ListenableObject()
-        this.elementValues = new ListenableObject()
-        this.histogramBinLimits = new ListenableObject()
-        this.histogramBinCounts = new ListenableObject()
-
-        this.randomNumber = new ListenableNumber()
-        this.startTime = new ListenableNumber()
+        super()
 
         this.randomNumberGetterIsBusy = new ControllableBoolean('randomNumberGetterIsBusy', this.requestParams)
-
-        this.stopButtonDisabled = new ElementBoolean('/stopButtonDisabled', this.elementValues, this.webSocketPathnames)
-        this.startButtonDisabled = new ElementBoolean('/startButtonDisabled', this.elementValues, this.webSocketPathnames)
-
-        this.randomNumberInnerText = new ElementString('/randomNumberInnerText', this.elementValues, this.webSocketPathnames)
-        this.startTimeInnerText = new ElementString('/startTimeInnerText', this.elementValues, this.webSocketPathnames)
-        this.histogramSVGInnerHTML = new ElementString('/histogramSVGInnerHTML', this.elementValues, this.webSocketPathnames)
     }
 }
 
