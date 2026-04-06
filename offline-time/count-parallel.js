6b75c66d6bd8e46f4eee114c67568e038fad922a
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-04-06 11:14:52 +0900

    added serial version code in count-parallel

--- offline-time/count-parallel.js
+++ offline-time/count-parallel.js
@@ -33,75 +33,31 @@ Promise.all(
         }))
     ).then(() => {
         performance.mark('exit')
-        console.log(`all exit ${performance.measure('', 'online', 'exit').duration}ms`)
+        console.log(`all exit ${performance.measure('', 'parallel', 'exit').duration}ms`)
     })
     // this is less than 1ms
     // performance.mark('online2')
     // console.log(`all listener ${performance.measure('', 'online', 'online2').duration}ms`)
-    workers.forEach(worker => worker.terminate())
+    createReadStream(filePath(), { highWaterMark: 16 * 1024 * 1024 }).on('data', chunk => {
+        for (let i = 0; i < chunk.length / 8; ++i) {
+            switch (chunk[8 * i]) {
+                case 0x5a:
+                    a++
+                    break
+                case 0x5b:
+                    b++
+                    break
+                case 0x5c:
+                    c++
+                    break
+                default:
+                    d++
+            }
+        }
+    }).on('close', () => {
+        performance.mark('parallel')
+        console.log(`a: ${a.toLocaleString()}, b: ${b.toLocaleString()}, c: ${c.toLocaleString()}, d: ${d.toLocaleString()}, ${performance.measure('', 'online', 'parallel').duration} ms`)
+        workers.forEach(worker => worker.terminate())
+    })
 })
-// workers.forEach((_, index) => {
-//     worker.on('online', () => {
-//         workers[index] = worker
-//         console.log(`worker${index} online`)
-//         if (workers.filter(worker => worker === null).length === 0) {
-
-//             performance.mark('worker')
-//             console.log(`all workers online. numWorkers: ${availableParallelism()} ${performance.measure('', 'start', 'worker').duration} ms`)
-//             createReadStream(filePath(), { highWaterMark: 16 * 1024 * 1024 }).on('data', chunk => {
-//                 for (let i = 0; i < chunk.length / 8; ++i) {
-//                     switch (chunk[8 * i]) {
-//                         case 0x5a:
-//                             a++
-//                             break
-//                         case 0x5b:
-//                             b++
-//                             break
-//                         case 0x5c:
-//                             c++
-//                             break
-//                         default:
-//                             d++
-//                     }
-//                 }
-//             }).on('close', () => {
-//                 performance.mark('serial')
-//                 console.log(`a: ${a.toLocaleString()}, b: ${b.toLocaleString()}, c: ${c.toLocaleString()}, d: ${d.toLocaleString()}, ${performance.measure('', 'worker', 'serial').duration} ms`)
-//                 a = 0
-//                 b = 0
-//                 c = 0
-//                 d = 0
-//                 createReadStream(filePath(), { highWaterMark: 16 * 1024 * 1024 }).on('data', chunk => {
-//                     for (let i = 0; i < chunk.length / 8; ++i) {
-//                         switch (chunk[8 * i]) {
-//                             case 0x5a:
-//                                 a++
-//                                 break
-//                             case 0x5b:
-//                                 b++
-//                                 break
-//                             case 0x5c:
-//                                 c++
-//                                 break
-//                             default:
-//                                 d++
-//                         }
-//                     }
-//                 }).on('close', () => {
-//                     performance.mark('parallel')
-//                     console.log(`a: ${a.toLocaleString()}, b: ${b.toLocaleString()}, c: ${c.toLocaleString()}, d: ${d.toLocaleString()}, ${performance.measure('', 'serial', 'parallel').duration} ms`)
-//                     workers[index].terminate()
-//                 })
-//             })
-//         }
-//     }).on('exit', () => {
-//         console.log(`worker${index} exit`)
-//         workers[index] = null
-//         if (workers.filter(worker => worker === null).length === 0) {
-//             performance.mark('shutdown')
-//             console.log(`shutdown completed ${performance.measure('', 'parallel', 'shutdown').duration} ms`)
-//             process.exit()
-//         }
-//     })
-// })
 
