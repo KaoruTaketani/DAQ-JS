cd8cae5e94fabcd6d7b954f656f21d5ec6b24e80
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-04-06 13:47:52 +0900

    stop using terminate in count-parallel and post null instead

--- offline-time/count-parallel.js
+++ offline-time/count-parallel.js
@@ -1,8 +1,9 @@
-import { Worker } from 'worker_threads'
-import { availableParallelism } from 'os'
 import { createReadStream } from 'fs'
-import filePath from './filePath.js'
+import { availableParallelism } from 'os'
 import { performance } from 'perf_hooks'
+import { Worker } from 'worker_threads'
+import partition from '../lib/partition.js'
+import filePath from './filePath.js'
 
 performance.mark('start')
 let a = 0,
@@ -10,10 +11,12 @@ let a = 0,
     c = 0,
     d = 0,
     workers = new Array(availableParallelism()).fill(null)
+const sharedBuffer = new SharedArrayBuffer(4 * 4)
+const sharedArray = new Uint32Array(sharedBuffer)
 
 Promise.all(
     workers.map((_, index) => new Promise(resolve => {
-        const worker = new Worker('./count-worker.js')
+        const worker = new Worker('./count-worker.js', { workerData: sharedBuffer })
         worker.on('online', () => {
             workers[index] = worker
             console.log(`worker${index} online`)
@@ -39,6 +42,14 @@ Promise.all(
     // performance.mark('online2')
     // console.log(`all listener ${performance.measure('', 'online', 'online2').duration}ms`)
     createReadStream(filePath(), { highWaterMark: 16 * 1024 * 1024 }).on('data', chunk => {
+        // const sharedBuffer = new SharedArrayBuffer(/** @type {Buffer} */(chunk).byteLength)
+        // const sharedArray = new Uint8Array(sharedBuffer)
+        // workers.forEach((worker, index) => {
+        //     const [start, end] = partition(chunk.length / 8, workers.length, index)
+        //     // worker.postMessage(chunk.slice(8 * start, 8 * end))
+        //     worker.postMessage(chunk.slice(8 * start, 8 * end))
+        // })
+
         for (let i = 0; i < chunk.length / 8; ++i) {
             switch (chunk[8 * i]) {
                 case 0x5a:
@@ -57,7 +68,9 @@ Promise.all(
     }).on('close', () => {
         performance.mark('parallel')
         console.log(`a: ${a.toLocaleString()}, b: ${b.toLocaleString()}, c: ${c.toLocaleString()}, d: ${d.toLocaleString()}, ${performance.measure('', 'online', 'parallel').duration} ms`)
-        workers.forEach(worker => worker.terminate())
+        // console.log(`sharedArray: ${sharedArray}, ${performance.measure('', 'online', 'parallel').duration} ms`)
+        workers.forEach(worker => worker.postMessage(null))
+        // workers.forEach(worker => worker.terminate())
     })
 })
 
--- offline-time/count-worker.js
+++ offline-time/count-worker.js
@@ -1,5 +1,33 @@
 import { parentPort } from 'worker_threads'
+import { workerData } from 'worker_threads'
+
+const sharedArray = new Uint32Array(workerData)
 
 parentPort.on('message', message => {
-
+    if (message === null) {
+        // console.log('exit')
+        process.exit(0)
+        return
+    }
+    const chunk = new Uint8Array(message)
+    // console.log(chunk.length)
+    for (let i = 0; i < chunk.length / 8; ++i) {
+        switch (chunk[8 * i]) {
+            case 0x5a:
+                // a++
+                Atomics.add(sharedArray, 0, 1)
+                break
+            case 0x5b:
+                // b++
+                Atomics.add(sharedArray, 1, 1)
+                break
+            case 0x5c:
+                // c++
+                Atomics.add(sharedArray, 2, 1)
+                break
+            default:
+                // d++
+                Atomics.add(sharedArray, 3, 1)
+        }
+    }
 })
