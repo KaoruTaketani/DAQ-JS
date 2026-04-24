e4781570d66818ffefa780a428b7565ed14b8688
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-04-24 11:15:42 +0900

    refactored centers handler

--- file-viewer/CentersHandler.js
+++ file-viewer/CentersHandler.js
@@ -55,64 +55,34 @@ export default class {
                 return
             }
             const xLimValues = this._url.searchParams.getAll('xLim')
-            // console.log(xLimValues)
             const yLimValues = this._url.searchParams.getAll('yLim')
-            // console.log(yLimValues)
-            if (xLimValues.length === 2 && yLimValues.length === 2) {
-                // console.log(filteredTOFHistogram.shape)
-                // console.log(filteredTOFHistogram.value)
-                const startTime = Date.now()
-                /** @type {Float64Array} */
-                const y =/** @type {Float64Array} */ (centers.value)
-                const x = colon(1, y.length)
-                const xLim = xLimValues.map(v => parseFloat(v))
-                const xTick = linspace(xLim[0], xLim[1], 8 + 1)
-                const yLim = yLimValues.map(v => parseFloat(v))
-                const ax = {
-                    xLim: xLim,
-                    yLim: yLim,
-                    xTick: xTick,
-                    yTick: yLim,
-                    xTickLabel: xTick.map(x => x.toFixed()),
-                    yTickLabel: yLimValues
-                }
-                // console.log(ax)
-                response.writeHead(200, { 'Content-Type': 'image/svg+xml' })
-                // console.log(colon(1, yData.length))
-                // console.log(yData)
-                response.end([
-                    axes(ax),
-                    xlabel(ax, 'horizontal coordinate (ch)'),
-                    line(ax, x, y)
-                ].join(''))
-                console.log(`elapsedTime: ${Date.now() - startTime}ms`)
-            } else {
-                // console.log(filteredTOFHistogram.shape)
-                // console.log(filteredTOFHistogram.value)
-                const startTime = Date.now()
-                /** @type {Float64Array} */
-                const y =/** @type {Float64Array} */ (centers.value)
-                const x = colon(1, y.length)
-                const yMax = max(y)
-                const xTick = linspace(0, y.length, 8 + 1)
-                const ax = {
-                    xLim: [0, y.length],
-                    yLim: [0, yMax],
-                    xTick: xTick,
-                    yTick: [0, yMax],
-                    xTickLabel: xTick.map(x => x.toFixed()),
-                    yTickLabel: ['0', `${yMax.toFixed()}`]
-                }
-                response.writeHead(200, { 'Content-Type': 'image/svg+xml' })
-                // console.log(colon(1, yData.length))
-                // console.log(yData)
-                response.end([
-                    axes(ax),
-                    xlabel(ax, 'horizontal coordinate (ch)'),
-                    line(ax, x, y)
-                ].join(''))
-                console.log(`elapsedTime: ${Date.now() - startTime}ms`)
+            const startTime = Date.now()
+            /** @type {Float64Array} */
+            const y =/** @type {Float64Array} */ (centers.value)
+            const x = colon(1, y.length)
+
+            const xLim = xLimValues.length === 2
+                ? xLimValues.map(v => parseFloat(v))
+                : [0, y.length]
+            const yLim = yLimValues.length === 2
+                ? yLimValues.map(v => parseFloat(v))
+                : [0, max(y)]
+            const xTick = linspace(xLim[0], xLim[1], 8 + 1)
+            const ax = {
+                xLim: xLim,
+                yLim: yLim,
+                xTick: xTick,
+                yTick: yLim,
+                xTickLabel: xTick.map(x => x.toFixed()),
+                yTickLabel: yLim.map(y => y.toString())
             }
+            response.writeHead(200, { 'Content-Type': 'image/svg+xml' })
+            response.end([
+                axes(ax),
+                xlabel(ax, 'horizontal coordinate (ch)'),
+                line(ax, x, y)
+            ].join(''))
+            console.log(`elapsedTime: ${Date.now() - startTime}ms`)
         }
     }
 }
