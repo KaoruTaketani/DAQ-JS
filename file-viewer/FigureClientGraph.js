1b423c96f376465983660d52bb726e19695920a3
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-05-22 11:46:58 +0900

    added cursor indicator in graph client

--- file-viewer/FigureClientGraph.js
+++ file-viewer/FigureClientGraph.js
@@ -1,3 +1,4 @@
+import CursorTextMaker from "./CursorTextMaker.js";
 import FigureCleanupperGraph from "./FigureCleanupperGraph.js";
 import FigureDrawerGraph from "./FigureDrawerGraph.js";
 import FigureGetterGraph from "./FigureGetterGraph.js";
@@ -8,6 +9,7 @@ import PathMaker from "./PathMaker.js";
 const variables = new FigureVariablesGraph()
 new PathMaker(variables)
 new FilesGetterHDF5(variables)
+new CursorTextMaker(variables)
 new FigureCleanupperGraph(variables)
 new FigureDrawerGraph(variables)
 new FigureGetterGraph(variables)
@@ -159,8 +161,30 @@ const listboxElement = document.createElement('select');
     element.setAttribute('width', '400')
     element.setAttribute('height', '300')
     element.setAttribute('viewBox', '0 0 560 420')
+    element.addEventListener('mousemove', ev => {
+        variables.cursorOffset.assign([ev.offsetX, ev.offsetY])
+    })
     variables.svgInnerHTML.addListener(arg => {
         element.innerHTML = arg
+
+        const axes =/** @type {HTMLElement} */(element.firstElementChild)
+        // console.log(`x: [${axes.dataset.xminInPixels}, ${axes.dataset.xmaxInPixels}], y: [${axes.dataset.yminInPixels}, ${axes.dataset.ymaxInPixels}]`)
+        if (axes.dataset.xminInPixels)
+            variables.xminInPixels.assign(parseInt(axes.dataset.xminInPixels))
+        if (axes.dataset.yminInPixels)
+            variables.yminInPixels.assign(parseInt(axes.dataset.yminInPixels))
+        if (axes.dataset.xmaxInPixels)
+            variables.xmaxInPixels.assign(parseInt(axes.dataset.xmaxInPixels))
+        if (axes.dataset.ymaxInPixels)
+            variables.ymaxInPixels.assign(parseInt(axes.dataset.ymaxInPixels))
+        if (axes.dataset.xminInData)
+            variables.xminInData.assign(parseFloat(axes.dataset.xminInData))
+        if (axes.dataset.yminInData)
+            variables.yminInData.assign(parseInt(axes.dataset.yminInData))
+        if (axes.dataset.xmaxInData)
+            variables.xmaxInData.assign(parseInt(axes.dataset.xmaxInData))
+        if (axes.dataset.ymaxInData)
+            variables.ymaxInData.assign(parseInt(axes.dataset.ymaxInData))
     })
 })(document.body.appendChild(document.createElementNS('http://www.w3.org/2000/svg', 'svg')));
 
