5feabc5d19d84a830d6275f4edd1a2a53f31d695
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-06-08 11:20:31 +0900

    refactored batch string

--- 13gc/BatchString.js
+++ 13gc/BatchString.js
@@ -3,22 +3,24 @@ import ListenableString from '../13/ListenableString.js'
 export default class extends ListenableString {
     constructor(key, requestParams, batchResolve) {
         super()
-        this._onceListeners = []
+        this._onceListener
         this._batchResolve
         batchResolve.addListener(arg => { this._batchResolve = arg })
         requestParams.addListener(arg => {
             if (!arg.has(key)) return
 
             super.assign(arg.get(key))
-            if (this._batchResolve) this._onceListeners.push(() => {
+            if (this._batchResolve) this._onceListener = () => {
                 this._batchResolve()
-            })
+            }
         })
     }
     assign(arg) {
         super.assign(arg)
-        this._onceListeners.forEach(onceListener => { onceListener(arg) })
-        this._onceListeners.splice(0)
+        if (this._onceListener) {
+            this._onceListener(arg)
+            this._onceListener = undefined
+        }
     }
 }
 
