94f2bab9877c03bafed5e3c8af3a3f038378cbaa
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-08-25 08:13:54 +0900

    use express in file-viewer and added RootRouter

---
+++ file-viewer/RootRouter.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+    res.send([
+        '<html>',
+        '<head>',
+        '    <meta charset="utf-8">',
+        '</head>',
+        '<body>',
+        `    <h2>EDR</h2>`,
+        `    <p><a href="./Table.html">Table</a></p>`,
+        `    <h2>HDF5</h2>`,
+        `    <p><a href="./Attributes.html">Attributes</a></p>`,
+        `    <p><a href="./XY.html">XY</a></p>`,
+        `    <p><a href="./Waveform.html">Waveform</a></p>`,
+        `    <p><a href="./Image.html">Image</a></p>`,
+        `    <h2>Calculator</h2>`,
+        `    <p><a href="./Slit.html">Slit</a></p>`,
+        `    <p><a href="./QRange.html">Q Range</a></p>`,
+        '</body>',
+        '</html>'
+    ].join('\n'))
+})
+
+export default router;
--- file-viewer/index.js
+++ file-viewer/index.js
@@ -1,82 +1,10 @@
-import { readFile } from 'fs'
-import { Server } from 'http'
-import { basename } from 'path'
-import AttributesHandler from './AttributesHandler.js'
-import FilesHandler from './FilesHandler.js'
-import NumEventsHandler from './NumEventsHandler.js'
-import RootHandler from './RootHandler.js'
-import Variables from './Variables.js'
-import XYHandler from './XYHandler.js'
-import WaveformHandler from './WaveformHandler.js'
-import ImageHandler from './ImageHandler.js'
-import TableHandler from './TableHandler.js'
+import express from 'express'
+import RootRouter from './RootRouter.js';
 
-const variables = new Variables()
-new FilesHandler(variables)
-new NumEventsHandler(variables)
-new AttributesHandler(variables)
-new RootHandler(variables)
-new XYHandler(variables)
-new WaveformHandler(variables)
-new ImageHandler(variables)
-new TableHandler(variables)
+const app = express()
 
-const httpServer = new Server()
-const responses = new Map()
-variables.responses.assign(responses)
-variables.edrPath.assign('../../edr')
-variables.hdf5Path.assign('../../hdf5')
+app.use('/', RootRouter)
 
-httpServer.on('request', (request, response) => {
-    console.log(`GET url: ${request.url}`)
-    const url = new URL(`http://localhost${request.url}`)
-
-    if (url.pathname.endsWith('.js')) {
-        if (url.pathname.startsWith('/lib/')) {
-            readFile(`../${request.url}`, 'utf8', (err, data) => {
-                if (err) throw err
-
-                response.writeHead(200, { 'Content-Type': 'text/javascript' })
-                response.end(data)
-            })
-        } else {
-            readFile(`.${request.url}`, 'utf8', (err, data) => {
-                if (err) throw err
-
-                response.writeHead(200, { 'Content-Type': 'text/javascript' })
-                response.end(data)
-            })
-        }
-        return
-    }
-    if (url.pathname.endsWith('.html')) {
-        if (url.pathname === '/Slit.html' ||
-            url.pathname === '/QRange.html' ||
-            url.pathname === '/Attributes.html' ||
-            url.pathname === '/Table.html' ||
-            url.pathname === '/Waveform.html' ||
-            url.pathname === '/Image.html' ||
-            url.pathname === '/XY.html') {
-            response.writeHead(200, { 'Content-Type': 'text/html' })
-            response.end([
-                '<html>',
-                '<head>',
-                '    <meta charset="utf-8">',
-                '</head>',
-                '<body>',
-                `    <script type="module" src="./${basename(url.pathname, '.html')}Client.js">`,
-                `    </script>`,
-                '</body>',
-                '</html>'
-            ].join('\n'))
-            return
-        } else {
-            response.writeHead(404)
-            response.end(`${url.pathname} was not found`)
-        }
-        return
-    }
-    responses.set(url, response)
-    // variables.response.assign(response)
-    variables.url.assign(url)
-}).listen(80)
+app.listen(80, () => {
+  console.log('Server is running on http://localhost')
+})
