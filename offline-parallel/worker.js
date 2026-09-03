1c395f970bc1a3188b68c1d9b91463563cd1b1db
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-09-03 15:35:31 +0900

    cleanup duplicated files from offline-parallel

--- offline-parallel/worker.js
+++ offline-parallel/worker.js
@@ -1,48 +1,9 @@
-import EventBufferParser from "./EventBufferParser.js"
-import FilteredImageInitializer from "./FilteredImageInitializer.js"
-import FilteredImageMaker from "./FilteredImageMaker.js"
-import FilteredNeutronEventMaker from "./FilteredNeutronEventMaker.js"
-import JSONFileReader from "./JSONFileReader.js"
-import NeutronEventMaker from "./NeutronEventMaker.js"
-import PairedEventMaker from "./PairedEventMaker.js"
-import PulseHeightHistogramInitializer from "./PulseHeightHistogramInitializer.js"
-import PulseHeightHistogramMaker from "./PulseHeightHistogramMaker.js"
-import ImageInitializer from "./ImageInitializer.js"
-import ImageMaker from "./ImageMaker.js"
-import TOFDifferenceHistogramInitializer from "./TOFDifferenceHistogramInitializer.js"
-import TOFDifferenceHistogramMaker from "./TOFDifferenceHistogramMaker.js"
-import TOFHistogramInitializer from "./TOFHistogramInitializer.js"
-import TOFHistogramMaker from "./TOFHistogramMaker.js"
-import WorkerVariables from "./WorkerVariables.js"
-import VerticalProjectionInitializer from "./VerticalProjectionInitializer.js"
-import VerticalProjectionMaker from "./VerticalProjectionMaker.js"
-import { MessagePort, parentPort } from 'worker_threads'
 import { ok } from 'assert'
-import { isTypedArray } from "util/types"
+import { parentPort } from 'worker_threads'
+import WorkerVariables from "./WorkerVariables.js"
 
 const variables = new WorkerVariables()
 
-new EventBufferParser(variables)
-new PairedEventMaker(variables)
-new NeutronEventMaker(variables)
-// new ImageInitializer(variables)
-new ImageMaker(variables)
-// new VerticalProjectionInitializer(variables)
-// new VerticalProjectionMaker(variables)
-// new FilteredNeutronEventMaker(variables)
-// new FilteredImageInitializer(variables)
-// new FilteredImageMaker(variables)
-// new TOFHistogramInitializer(variables)
-new TOFHistogramMaker(variables)
-// new TOFImageInitializer(variables)
-// new TOFImageMaker(variables)
-// new HorizontalProjectionHistogramsInitializer(variables)
-// new HorizontalProjectionHistogramsMaker(variables)
-// new PulseHeightHistogramInitializer(variables)
-// new PulseHeightHistogramMaker(variables)
-// new TOFDifferenceHistogramInitializer(variables)
-// new TOFDifferenceHistogramMaker(variables)
-// new JSONFileReader(variables)
 
 ok(parentPort)
 parentPort.on('message', message => {
