d94bf7ca11cf858c939a749f5e86d3c4f3a22e2f
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-09-11 10:58:17 +0900

    use imagesc and imwrite and cleanup image.js from online-neunet

--- online-neunet/ImageSrcMaker.js
+++ online-neunet/ImageSrcMaker.js
@@ -1,6 +1,7 @@
-import throttle from '../lib/throttle.js'
+import imagesc from '../lib/imagesc.js'
+import imwrite from '../lib/imwrite.js'
 import max from '../lib/max.js'
-import image from './image.js'
+import throttle from '../lib/throttle.js'
 import Operator from './Operator.js'
 
 export default class extends Operator {
@@ -16,15 +17,9 @@ export default class extends Operator {
             this._operation()
         })
         this._operation = throttle(() => {
-            /** @type {import('../lib/index.js').Axes} */
-            const ax = {
-                xLim: [0, 1], xTick: [0, 1], xTickLabel: ['0', '1'],
-                yLim: [0, 1], yTick: [0, 1], yTickLabel: ['0', '1'],                
-                zLim: [0, max(this._image.data)]
-            }
-            image(ax, this._image).then(buf => {
+            imwrite(imagesc(this._image)).then(buf => {
                 variables.imageSrc.assign(`data:image/png;base64,${buf.toString('base64')}`)
             })
-        },1000)
+        }, 1000)
     }
 }
