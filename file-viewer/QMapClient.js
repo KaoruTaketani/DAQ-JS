75198bb62d6e97b9b055d1205eeed4d373f1c4e4
Kaoru Taketani <kaoru.taketani@gmail.com>
2026-09-14 13:26:53 +0900

    stopped using table in qmap

--- file-viewer/QMapClient.js
+++ file-viewer/QMapClient.js
@@ -5,152 +5,113 @@ const variables = new QMapVariables()
 new QMapDrawer(variables);
 
 (element => {
+    element.style.display = 'flex';
+    element.appendChild(document.createTextNode('camera width (mm)'));
     (element => {
-        (element => {
-            (element => {
-                element.innerText = 'camera width (mm)'
-            })(element.appendChild(document.createElement('th')));
-            (element => {
-                (element => {
-                    element.type = 'number'
-                    element.style.width = '100px'
-                    element.addEventListener('change', () => {
-                        variables.cameraWidthInMillimeters.assign(element.value)
-                    })
-                    variables.cameraWidthInMillimeters.addListener(arg => {
-                        element.value = arg
-                    })
-                })(element.appendChild(document.createElement('input')))
-            })(element.appendChild(document.createElement('td')));
-        })(element.appendChild(document.createElement('tr')));
+        element.type = 'number'
+        element.style.width = '100px'
+        element.addEventListener('change', () => {
+            variables.cameraWidthInMillimeters.assign(element.value)
+        })
+        variables.cameraWidthInMillimeters.addListener(arg => {
+            element.value = arg
+        })
+    })(element.appendChild(document.createElement('input')))
+})(document.body.appendChild(document.createElement('label')));
 
-        (element => {
-            (element => {
-                element.innerText = 'camera length (m)'
-            })(element.appendChild(document.createElement('th')));
-            (element => {
-                (element => {
-                    element.type = 'number'
-                    element.style.width = '100px'
-                    element.addEventListener('change', () => {
-                        variables.cameraLengthInMeters.assign(element.value)
-                    })
-                    variables.cameraLengthInMeters.addListener(arg => {
-                        element.value = arg
-                    })
-                })(element.appendChild(document.createElement('input')))
-            })(element.appendChild(document.createElement('td')));
-        })(element.appendChild(document.createElement('tr')));
+(element => {
+    element.style.display = 'flex';
+    element.appendChild(document.createTextNode('camera length (m)'));
+    (element => {
+        element.type = 'number'
+        element.style.width = '100px'
+        element.addEventListener('change', () => {
+            variables.cameraLengthInMeters.assign(element.value)
+        })
+        variables.cameraLengthInMeters.addListener(arg => {
+            element.value = arg
+        })
+    })(element.appendChild(document.createElement('input')))
+})(document.body.appendChild(document.createElement('label')));
 
-        (element => {
-            (element => {
-                element.innerText = 'moderator to sample (m)'
-            })(element.appendChild(document.createElement('th')));
-            (element => {
-                (element => {
-                    element.type = 'number'
-                    element.style.width = '100px'
-                    element.addEventListener('change', () => {
-                        variables.moderatorToSampleDistanceInMeters.assign(element.value)
-                    })
-                    variables.moderatorToSampleDistanceInMeters.addListener(arg => {
-                        element.value = arg
-                    })
-                })(element.appendChild(document.createElement('input')))
-            })(element.appendChild(document.createElement('td')));
-        })(element.appendChild(document.createElement('tr')));
+(element => {
+    element.style.display = 'flex';
+    element.appendChild(document.createTextNode('moderator to sample (m)'));
+    (element => {
+        element.type = 'number'
+        element.style.width = '100px'
+        element.addEventListener('change', () => {
+            variables.moderatorToSampleDistanceInMeters.assign(element.value)
+        })
+        variables.moderatorToSampleDistanceInMeters.addListener(arg => {
+            element.value = arg
+        })
+    })(element.appendChild(document.createElement('input')))
+})(document.body.appendChild(document.createElement('label')));
 
-        (element => {
-            (element => {
-                element.innerText = 'tof min (ms)'
-            })(element.appendChild(document.createElement('th')));
-            (element => {
-                (element => {
-                    element.type = 'number'
-                    element.style.width = '100px'
-                    element.addEventListener('change', () => {
-                        variables.tofMinInMilliseconds.assign(element.value)
-                    })
-                    variables.tofMinInMilliseconds.addListener(arg => {
-                        element.value = arg
-                    })
-                })(element.appendChild(document.createElement('input')))
-            })(element.appendChild(document.createElement('td')));
-        })(element.appendChild(document.createElement('tr')));
+(element => {
+    element.style.display = 'flex';
+    element.appendChild(document.createTextNode('tof min (ms)'));
+    (element => {
+        element.type = 'number'
+        element.style.width = '100px'
+        element.addEventListener('change', () => {
+            variables.tofMinInMilliseconds.assign(element.value)
+        })
+        variables.tofMinInMilliseconds.addListener(arg => {
+            element.value = arg
+        })
+    })(element.appendChild(document.createElement('input')))
+})(document.body.appendChild(document.createElement('label')));
 
-        (element => {
-            (element => {
-                element.innerText = 'tof max (ms)'
-            })(element.appendChild(document.createElement('th')));
-            (element => {
-                (element => {
-                    element.type = 'number'
-                    element.style.width = '100px'
-                    element.addEventListener('change', () => {
-                        variables.tofMaxInMilliseconds.assign(element.value)
-                    })
-                    variables.tofMaxInMilliseconds.addListener(arg => {
-                        element.value = arg
-                    })
-                })(element.appendChild(document.createElement('input')))
-            })(element.appendChild(document.createElement('td')));
-        })(element.appendChild(document.createElement('tr')));
+(element => {
+    element.style.display = 'flex';
+    element.appendChild(document.createTextNode('tof max (ms)'));
+    (element => {
+        element.type = 'number'
+        element.style.width = '100px'
+        element.addEventListener('change', () => {
+            variables.tofMaxInMilliseconds.assign(element.value)
+        })
+        variables.tofMaxInMilliseconds.addListener(arg => {
+            element.value = arg
+        })
+    })(element.appendChild(document.createElement('input')))
+})(document.body.appendChild(document.createElement('label')));
 
-        (element => {
-            (element => {
-                element.innerText = 'incident angle (deg)'
-            })(element.appendChild(document.createElement('th')));
-            (element => {
-                (element => {
-                    element.type = 'number'
-                    element.style.width = '100px'
-                    element.step = '0.1'
-                    element.addEventListener('change', () => {
-                        variables.incidentAngleInDegrees.assign(element.value)
-                    })
-                    variables.incidentAngleInDegrees.addListener(arg => {
-                        element.value = arg
-                    })
-                })(element.appendChild(document.createElement('input')))
-            })(element.appendChild(document.createElement('td')));
-        })(element.appendChild(document.createElement('tr')));
+(element => {
+    element.style.display = 'flex';
+    element.appendChild(document.createTextNode('incident angle (deg)'));
+    (element => {
+        element.type = 'number'
+        element.style.width = '100px'
+        element.step = '0.1'
+        element.addEventListener('change', () => {
+            variables.incidentAngleInDegrees.assign(element.value)
+        })
+        variables.incidentAngleInDegrees.addListener(arg => {
+            element.value = arg
+        })
+    })(element.appendChild(document.createElement('input')))
+})(document.body.appendChild(document.createElement('label')));
 
-        (element => {
-            (element => {
-                element.innerText = 'wavelength min (Å)'
-            })(element.appendChild(document.createElement('th')));
-            (element => {
-                variables.wavelengthMinInAngstroms.addListener(arg => {
-                    element.innerText = arg
-                })
-            })(element.appendChild(document.createElement('td')));
-        })(element.appendChild(document.createElement('tr')));
+(element => {
+    variables.wavelengthMinInAngstroms.addListener(arg => {
+        element.innerText = `wavelength min: ${arg} Å`
+    })
+})(document.body.appendChild(document.createElement('div')));
 
-        (element => {
-            (element => {
-                element.innerText = 'wavelength max (Å)'
-            })(element.appendChild(document.createElement('th')));
-            (element => {
-                variables.wavelengthMaxInAngstroms.addListener(arg => {
-                    element.innerText = arg
-                })
-            })(element.appendChild(document.createElement('td')));
-        })(element.appendChild(document.createElement('tr')));
+(element => {
+    variables.wavelengthMaxInAngstroms.addListener(arg => {
+        element.innerText = `wavelength max: ${arg} Å`
+    })
+})(document.body.appendChild(document.createElement('div')));
 
-        (element => {
-            (element => {
-                element.innerText = 'sample width (mm)'
-            })(element.appendChild(document.createElement('th')));
-            (element => {
-                variables.sampleWidthInMillimeters.addListener(arg => {
-                    element.innerText = arg
-                })
-            })(element.appendChild(document.createElement('td')));
-        })(element.appendChild(document.createElement('tr')));
-
-    })(element.appendChild(document.createElement('tbody')));
-})(document.body.appendChild(document.createElement('table')));
-
+(element => {
+    variables.sampleWidthInMillimeters.addListener(arg => {
+        element.innerText = `sample width: ${arg} mm`
+    })
+})(document.body.appendChild(document.createElement('div')));
 
 
 (element => {
